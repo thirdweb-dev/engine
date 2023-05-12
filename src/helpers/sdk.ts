50c55b152201351e7f398374649f0dcc4f509ef0
Farhan Khwaja <farhan@thirdweb.com>
2023-05-12 16:37:39 -0700

    fixed build error

--- src/helpers/sdk.ts
+++ src/helpers/sdk.ts
@@ -14,7 +14,7 @@ export async function getSDK(chainName: ChainOrRpc): Promise<ThirdwebSDK> {
 
   // Need to make this instantiate SDK with read/write. For that will need wallet information
   // Currently only doing read-only mode as per Docs
-  if (getEnv('NODE_ENV') === 'local'){)
+  if (getEnv('NODE_ENV') === 'local'){
     sdkMap[chainName] = await ThirdwebSDK.fromPrivateKey(getEnv('WALLET_PRIVATE_KEY'), chainName, {thirdwebApiKey: getEnv('API_KEY')});  
   }
   // else if (getEnv('USE_WALLET') === 'true') {
