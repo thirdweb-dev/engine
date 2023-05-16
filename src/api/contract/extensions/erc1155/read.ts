b9d3031309e0a44f5f57e6ab414623b5f0dc8014
Farhan Khwaja <farhan@thirdweb.com>
2023-05-16 16:31:35 -0700

    commented ERC721 & ERC1155 extensions to avoid build error

--- src/api/contract/extensions/erc1155/read.ts
+++ src/api/contract/extensions/erc1155/read.ts
@@ -1,42 +1,42 @@
-import { FastifyInstance, RouteGenericInterface } from 'fastify';
-import { StatusCodes } from 'http-status-codes';
-import { getSDK } from '../../../../helpers/sdk';
-import { Static } from '@sinclair/typebox';
-import { fullRouteSchema, schemaTypes } from '../../../../sharedApiSchemas';
-import { logger } from '../../../../utilities/logger';
+// import { FastifyInstance, RouteGenericInterface } from 'fastify';
+// import { StatusCodes } from 'http-status-codes';
+// import { getSDK } from '../../../../helpers/sdk';
+// import { Static } from '@sinclair/typebox';
+// import { fullRouteSchema, schemaTypes } from '../../../../sharedApiSchemas';
+// import { logger } from '../../../../utilities/logger';
 
-export async function erc1155ReadContract(fastify: FastifyInstance) {
-  fastify.route<schemaTypes>({
-    method: 'GET',
-    url: '/contract/:chain_name_or_id/:contract_address/erc1155/read',
-    schema: {
-      description: 'Read From ERC1155 Contract',
-      tags: ['extensions'],
-      operationId: 'erc1155Read',
-      ...fullRouteSchema,
-    },
-    handler: async (request, reply) => {
-      const { chain_name_or_id, contract_address } = request.params;
-      const { function_name, args } = request.query;
-      logger.info('Inside Read Function');
-      logger.silly(`Chain : ${chain_name_or_id}`)
-      logger.silly(`Contract Address : ${contract_address}`);
+// export async function erc1155ReadContract(fastify: FastifyInstance) {
+//   fastify.route<schemaTypes>({
+//     method: 'GET',
+//     url: '/contract/:chain_name_or_id/:contract_address/erc1155/read',
+//     schema: {
+//       description: 'Read From ERC1155 Contract',
+//       tags: ['extensions'],
+//       operationId: 'erc1155Read',
+//       ...fullRouteSchema,
+//     },
+//     handler: async (request, reply) => {
+//       const { chain_name_or_id, contract_address } = request.params;
+//       const { function_name, args } = request.query;
+//       logger.info('Inside Read Function');
+//       logger.silly(`Chain : ${chain_name_or_id}`)
+//       logger.silly(`Contract Address : ${contract_address}`);
 
-      logger.silly(`Function Name : ${function_name}`)
-      logger.silly(`Args : ${args}`);
+//       logger.silly(`Function Name : ${function_name}`)
+//       logger.silly(`Args : ${args}`);
 
-      const sdk = await getSDK(chain_name_or_id);
-      const contract = await sdk.getContract(contract_address);
+//       const sdk = await getSDK(chain_name_or_id);
+//       const contract = await sdk.getContract(contract_address);
 
-      const returnData: any = "adad";
-      // await contract.erc1155.call(function_name, args ? args.split(',') : []);
+//       const returnData: any = "adad";
+//       // await contract.erc1155.call(function_name, args ? args.split(',') : []);
       
-      reply.status(StatusCodes.OK).send({
-        result: {
-          data: returnData
-        },
-        error: null,
-      });
-    },
-  });
-}
+//       reply.status(StatusCodes.OK).send({
+//         result: {
+//           data: returnData
+//         },
+//         error: null,
+//       });
+//     },
+//   });
+// }
--- src/api/contract/extensions/erc1155/write.ts
+++ src/api/contract/extensions/erc1155/write.ts
@@ -1,44 +1,44 @@
-import { FastifyInstance, RouteGenericInterface } from 'fastify';
-import { StatusCodes } from 'http-status-codes';
-import { getSDK } from '../../../../helpers/sdk';
-import { Static } from '@sinclair/typebox';
-import { fullRouteSchema, schemaTypes } from '../../../../sharedApiSchemas';
-import { logger } from '../../../../utilities/logger';
+// import { FastifyInstance, RouteGenericInterface } from 'fastify';
+// import { StatusCodes } from 'http-status-codes';
+// import { getSDK } from '../../../../helpers/sdk';
+// import { Static } from '@sinclair/typebox';
+// import { fullRouteSchema, schemaTypes } from '../../../../sharedApiSchemas';
+// import { logger } from '../../../../utilities/logger';
 
-export async function erc1155WriteContract(fastify: FastifyInstance) {
-  fastify.route<schemaTypes>({
-    method: 'POST',
-    url: '/contract/:chain_name_or_id/:contract_address/erc1155/write',
-    schema: {
-      description: 'Write To ERC1155 Contract',
-      tags: ['extensions'],
-      operationId: 'erc1155Write',
-      ...fullRouteSchema,
-    },
-    handler: async (request, reply) => {
-      const { chain_name_or_id, contract_address } = request.params;
-      const { function_name, args } = request.query;
+// export async function erc1155WriteContract(fastify: FastifyInstance) {
+//   fastify.route<schemaTypes>({
+//     method: 'POST',
+//     url: '/contract/:chain_name_or_id/:contract_address/erc1155/write',
+//     schema: {
+//       description: 'Write To ERC1155 Contract',
+//       tags: ['extensions'],
+//       operationId: 'erc1155Write',
+//       ...fullRouteSchema,
+//     },
+//     handler: async (request, reply) => {
+//       const { chain_name_or_id, contract_address } = request.params;
+//       const { function_name, args } = request.query;
       
-      logger.info('Inside Write Function');
-      logger.silly(`Chain : ${chain_name_or_id}`)
-      logger.silly(`Contract Address : ${contract_address}`);
+//       logger.info('Inside Write Function');
+//       logger.silly(`Chain : ${chain_name_or_id}`)
+//       logger.silly(`Contract Address : ${contract_address}`);
 
-      logger.silly(`Function Name : ${function_name}`)
-      logger.silly(`Contract Address : ${contract_address}`);
-      logger.silly(`Function Arguments : ${args}`);
+//       logger.silly(`Function Name : ${function_name}`)
+//       logger.silly(`Contract Address : ${contract_address}`);
+//       logger.silly(`Function Arguments : ${args}`);
 
-      const sdk = await getSDK(chain_name_or_id);
-      const contract:any = await sdk.getContract(contract_address);
+//       const sdk = await getSDK(chain_name_or_id);
+//       const contract:any = await sdk.getContract(contract_address);
       
-      const returnData: any = "asdad";
-      // await contract.call(function_name, args ? args.split(',') : []);
+//       const returnData: any = "asdad";
+//       // await contract.call(function_name, args ? args.split(',') : []);
       
-      reply.status(StatusCodes.OK).send({
-        result: {
-          transaction: returnData?.receipt
-        },
-        error: null,
-      });
-    },
-  });
-}
+//       reply.status(StatusCodes.OK).send({
+//         result: {
+//           transaction: returnData?.receipt
+//         },
+//         error: null,
+//       });
+//     },
+//   });
+// }
