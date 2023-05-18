bdc2f0ac95b0a2860ff5a71610ec304101186549
Joaquim Verges <joaquim.verges@gmail.com>
2023-05-18 15:38:57 -0700

    update SDK, body params for transfer, prettier

--- src/api/contract/extensions/erc20/standard/transfer.ts
+++ src/api/contract/extensions/erc20/standard/transfer.ts
@@ -1,41 +1,45 @@
-import { FastifyInstance } from 'fastify';
-import { StatusCodes } from 'http-status-codes';
+import { FastifyInstance } from "fastify";
+import { StatusCodes } from "http-status-codes";
 
-import { getSDK } from '../../../../../helpers/sdk';
-import { partialRouteSchema } from '../../../../../sharedApiSchemas';
-import { logger } from '../../../../../utilities/logger';
-import { transferRequestBodySchema, transferRouteSchema } from '../../../../../schemas/erc20/standard/transfer';
+import { getSDK } from "../../../../../helpers/sdk";
+import { partialRouteSchema } from "../../../../../sharedApiSchemas";
+import { logger } from "../../../../../utilities/logger";
+import {
+  transferRequestBodySchema,
+  transferRouteSchema,
+} from "../../../../../schemas/erc20/standard/transfer";
 
 export async function erc20Transfer(fastify: FastifyInstance) {
   fastify.route<transferRouteSchema>({
-    method: 'POST',
-    url: '/contract/:chain_name_or_id/:contract_address/erc20/transfer',
+    method: "POST",
+    url: "/contract/:chain_name_or_id/:contract_address/erc20/transfer",
     schema: {
-      description: 'Transfer tokens from the connected (Admin) wallet to another wallet.',
-      tags: ['ERC20'],
-      operationId: 'transfer',
+      description:
+        "Transfer tokens from the connected (Admin) wallet to another wallet.",
+      tags: ["ERC20"],
+      operationId: "transfer",
       ...partialRouteSchema,
-      querystring: transferRequestBodySchema
+      body: transferRequestBodySchema,
     },
     handler: async (request, reply) => {
       const { chain_name_or_id, contract_address } = request.params;
       const { to_address, amount } = request.body;
-      logger.info('Inside ERC20 - Transfer Function');
-      logger.silly(`Chain : ${chain_name_or_id}`)
+      logger.info("Inside ERC20 - Transfer Function");
+      logger.silly(`Chain : ${chain_name_or_id}`);
       logger.silly(`Contract Address : ${contract_address}`);
 
-      logger.silly(`To Address : ${to_address}`)
+      logger.silly(`To Address : ${to_address}`);
       logger.silly(`Amount : ${amount}`);
 
       const sdk = await getSDK(chain_name_or_id);
       const contract = await sdk.getContract(contract_address);
 
       const returnData: any = await contract.erc20.transfer(to_address, amount);
-      
+
       reply.status(StatusCodes.OK).send({
         result: {
-          transaction: returnData?.receipt
-        }
+          transaction: returnData?.receipt,
+        },
       });
     },
   });
--- src/api/contract/extensions/erc20/standard/transferFrom.ts
+++ src/api/contract/extensions/erc20/standard/transferFrom.ts
@@ -1,42 +1,49 @@
-import { FastifyInstance } from 'fastify';
-import { StatusCodes } from 'http-status-codes';
+import { FastifyInstance } from "fastify";
+import { StatusCodes } from "http-status-codes";
 
-import { getSDK } from '../../../../../helpers/sdk';
-import { partialRouteSchema } from '../../../../../sharedApiSchemas';
-import { logger } from '../../../../../utilities/logger';
-import { transferFromRequestBodySchema, transferFromRouteSchema } from '../../../../../schemas/erc20/standard/transferFrom';
-
+import { getSDK } from "../../../../../helpers/sdk";
+import { partialRouteSchema } from "../../../../../sharedApiSchemas";
+import { logger } from "../../../../../utilities/logger";
+import {
+  transferFromRequestBodySchema,
+  transferFromRouteSchema,
+} from "../../../../../schemas/erc20/standard/transferFrom";
 
 export async function erc20TransferFrom(fastify: FastifyInstance) {
   fastify.route<transferFromRouteSchema>({
-    method: 'POST',
-    url: '/contract/:chain_name_or_id/:contract_address/erc20/transferFrom',
+    method: "POST",
+    url: "/contract/:chain_name_or_id/:contract_address/erc20/transferFrom",
     schema: {
-      description: 'Transfer tokens from the connected wallet to another wallet.',
-      tags: ['ERC20'],
-      operationId: 'transferFrom',
+      description:
+        "Transfer tokens from the connected wallet to another wallet.",
+      tags: ["ERC20"],
+      operationId: "transferFrom",
       ...partialRouteSchema,
-      body: transferFromRequestBodySchema
+      body: transferFromRequestBodySchema,
     },
     handler: async (request, reply) => {
       const { chain_name_or_id, contract_address } = request.params;
       const { from_address, to_address, amount } = request.body;
-      logger.info('Inside ERC20 - Transfer Function');
-      logger.silly(`Chain : ${chain_name_or_id}`)
+      logger.info("Inside ERC20 - Transfer Function");
+      logger.silly(`Chain : ${chain_name_or_id}`);
       logger.silly(`Contract Address : ${contract_address}`);
 
-      logger.silly(`To Address : ${to_address}`)
+      logger.silly(`To Address : ${to_address}`);
       logger.silly(`Amount : ${amount}`);
 
       const sdk = await getSDK(chain_name_or_id);
       const contract = await sdk.getContract(contract_address);
 
-      const returnData: any = await contract.erc20.transferFrom(from_address, to_address, amount);
-      
+      const returnData: any = await contract.erc20.transferFrom(
+        from_address,
+        to_address,
+        amount,
+      );
+
       reply.status(StatusCodes.OK).send({
         result: {
-          transaction: returnData?.receipt
-        }
+          transaction: returnData?.receipt,
+        },
       });
     },
   });
