2862fae2bb66daa718e9311ba922bff496ac9372
Joaquim Verges <joaquim.verges@gmail.com>
2023-05-19 16:14:52 -0700

    deploy prebuilt endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,15 @@
-import { FastifyInstance } from 'fastify';
-import { readContract } from './contract/read/read';
-import { writeToContract } from './contract/write/write';
+import { FastifyInstance } from "fastify";
+import { readContract } from "./contract/read/read";
+import { writeToContract } from "./contract/write/write";
 
 // Extensions
-import { erc20Routes } from './contract/extensions/erc20/index';
+import { erc20Routes } from "./contract/extensions/erc20/index";
+import { deployPrebuilt } from "./deployer/prebuilt";
 
 export const apiRoutes = async (fastify: FastifyInstance) => {
+  await fastify.register(deployPrebuilt);
+
+  // generic
   await fastify.register(readContract);
   await fastify.register(writeToContract);
 
---
+++ src/schemas/deployer/prebuilt.ts
@@ -0,0 +1,33 @@
+import { Static, Type } from "@sinclair/typebox";
+import { deployerSchemaTypes } from "../../sharedApiSchemas";
+
+export const deployPrebuiltRequestBodySchema = Type.Object({
+  // TODO need to type this
+  contractMetadata: Type.Any({
+    description: "Arguments for the deployment.",
+  }),
+  version: Type.Optional(
+    Type.String({
+      description: "Version of the contract to deploy. Defaults to latest.",
+    }),
+  ),
+});
+
+// Adding example for Swagger File
+deployPrebuiltRequestBodySchema.examples = [
+  {
+    contractMetadata: {
+      name: `My Contract`,
+      description: "Contract deployed from web3 api",
+      primary_sale_recipient: "0x1946267d81Fb8aDeeEa28e6B98bcD446c8248473",
+      seller_fee_basis_points: 500,
+      fee_recipient: "0x1946267d81Fb8aDeeEa28e6B98bcD446c8248473",
+      platform_fee_basis_points: 10,
+      platform_fee_recipient: "0x1946267d81Fb8aDeeEa28e6B98bcD446c8248473",
+    },
+  },
+];
+
+export interface deployPrebuiltSchema extends deployerSchemaTypes {
+  Body: Static<typeof deployPrebuiltRequestBodySchema>;
+}
