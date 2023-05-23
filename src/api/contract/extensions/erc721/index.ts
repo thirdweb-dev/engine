d1a1135dd186d4b88f7bea280f26a75108792ee6
Joaquim Verges <joaquim.verges@gmail.com>
2023-05-23 16:30:07 -0700

    erc721 isApproved

--- src/api/contract/extensions/erc721/index.ts
+++ src/api/contract/extensions/erc721/index.ts
@@ -2,12 +2,14 @@ import { FastifyInstance } from "fastify";
 import { erc721Get } from "./read/get";
 import { erc721GetAll } from "./read/getAll";
 import { erc721BalanceOf } from "./read/balanceOf";
+import { erc721isApproved } from "./read/isApproved";
 
 export const erc721Routes = async (fastify: FastifyInstance) => {
   // GET
   await fastify.register(erc721Get);
   await fastify.register(erc721GetAll);
   await fastify.register(erc721BalanceOf);
+  await fastify.register(erc721isApproved);
 
   // POST
 };
---
+++ src/api/contract/extensions/erc721/read/isApproved.ts
@@ -0,0 +1,65 @@
+import { FastifyInstance } from "fastify";
+import { StatusCodes } from "http-status-codes";
+
+import { getSDK } from "../../../../../helpers/index";
+import {
+  baseReplyErrorSchema,
+  contractParamSchema,
+} from "../../../../../helpers/sharedApiSchemas";
+import { Static, Type } from "@sinclair/typebox";
+
+// INPUTS
+const requestSchema = contractParamSchema;
+const querystringSchema = Type.Object({
+  owner_wallet: Type.String({
+    description: "Address of the wallet who owns the NFT",
+    examples: ["0x3EcDBF3B911d0e9052b64850693888b008e18373"],
+  }),
+  operator: Type.String({
+    description: "Address of the operator to check approval on",
+    examples: ["0x1946267d81Fb8aDeeEa28e6B98bcD446c8248473"],
+  }),
+});
+
+// OUTPUT
+const responseSchema = Type.Object({
+  result: Type.Optional(Type.Boolean()),
+  error: Type.Optional(baseReplyErrorSchema),
+});
+
+// LOGIC
+export async function erc721isApproved(fastify: FastifyInstance) {
+  fastify.route<{
+    Params: Static<typeof requestSchema>;
+    Reply: Static<typeof responseSchema>;
+    Querystring: Static<typeof querystringSchema>;
+  }>({
+    method: "GET",
+    url: "/contract/:chain_name_or_id/:contract_address/erc721/isApproved",
+    schema: {
+      description:
+        "Get whether this wallet has approved transfers from the given operator.",
+      tags: ["ERC721"],
+      operationId: "erc721_isApproved",
+      params: requestSchema,
+      querystring: querystringSchema,
+      response: {
+        [StatusCodes.OK]: responseSchema,
+      },
+    },
+    handler: async (request, reply) => {
+      const { chain_name_or_id, contract_address } = request.params;
+      const { owner_wallet, operator } = request.query;
+      const sdk = await getSDK(chain_name_or_id);
+      const contract = await sdk.getContract(contract_address);
+      const returnData: any = await contract.erc721.isApproved(
+        owner_wallet,
+        operator,
+      );
+
+      reply.status(StatusCodes.OK).send({
+        result: returnData,
+      });
+    },
+  });
+}
