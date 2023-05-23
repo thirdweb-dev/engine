60489bf5f98da95828b5bcc264fd8007b81e37d5
Joaquim Verges <joaquim.verges@gmail.com>
2023-05-23 16:46:00 -0700

    erc721 getOwned, claimed, unclaimed supply

--- src/api/contract/extensions/erc721/index.ts
+++ src/api/contract/extensions/erc721/index.ts
@@ -4,14 +4,20 @@ import { erc721GetAll } from "./read/getAll";
 import { erc721BalanceOf } from "./read/balanceOf";
 import { erc721IsApproved } from "./read/isApproved";
 import { erc721TotalCount } from "./read/totalCount";
+import { erc721TotalClaimedSupply } from "./read/totalClaimedSupply";
+import { erc721GetOwned } from "./read/getOwned";
+import { erc721TotalUnclaimedSupply } from "./read/totalUnclaimedSupply";
 
 export const erc721Routes = async (fastify: FastifyInstance) => {
   // GET
   await fastify.register(erc721Get);
   await fastify.register(erc721GetAll);
+  await fastify.register(erc721GetOwned);
   await fastify.register(erc721BalanceOf);
   await fastify.register(erc721IsApproved);
   await fastify.register(erc721TotalCount);
+  await fastify.register(erc721TotalClaimedSupply);
+  await fastify.register(erc721TotalUnclaimedSupply);
 
   // POST
 };
