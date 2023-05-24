190d6365f1a28987275627987347b7db823a28af
Joaquim Verges <joaquim.verges@gmail.com>
2023-05-24 16:55:07 -0700

    ssl flag

--- src/helpers/database/dbConnect.ts
+++ src/helpers/database/dbConnect.ts
@@ -1,99 +1,105 @@
-import { Knex } from 'knex';
-import pg from 'knex';
-import { getEnv } from '../loadEnv';
-import { FastifyInstance, FastifyRequest } from 'fastify';
+import { Knex } from "knex";
+import pg from "knex";
+import { getEnv } from "../loadEnv";
+import { FastifyInstance, FastifyRequest } from "fastify";
 
-const DATABASE_NAME = getEnv('DATABASE_NAME');
+const DATABASE_NAME = getEnv("DATABASE_NAME");
 
 // Defaults to postgres
-const dbClient = getEnv('DATABASE_CLIENT') ?? 'pg';
+const dbClient = getEnv("DATABASE_CLIENT") ?? "pg";
 
-export const connectToDB = async (server: FastifyInstance | FastifyRequest) : Promise<Knex> => {
+export const connectToDB = async (
+  server: FastifyInstance | FastifyRequest,
+): Promise<Knex> => {
+  // Creating KNEX Config
+  let connection: Knex.PgConnectionConfig = {
+    host: getEnv("POSTGRES_HOST"),
+    user: getEnv("POSTGRES_USER"),
+    password: getEnv("POSTGRES_PASSWORD"),
+    database: "",
+    ssl: true,
+  };
 
-    // Creating KNEX Config
-    let connection: Knex.ConnectionConfig = {
-        host: getEnv('POSTGRES_HOST'),
-        user: getEnv('POSTGRES_USER'),
-        password: getEnv('POSTGRES_PASSWORD'),
-        database: ''
-    };
+  let knexConfig: Knex.Config = {
+    client: dbClient,
+    connection,
+  };
 
-    let knexConfig: Knex.Config = {
-        client: dbClient,
-        connection,
-    };
+  // Set the appropriate databse client package
+  let dbClientPackage: any;
+  switch (dbClient) {
+    case "pg":
+      dbClientPackage = pg;
+      break;
+    default:
+      throw new Error(`Unsupported database client: ${dbClient}`);
+  }
 
-    // Set the appropriate databse client package
-    let dbClientPackage: any;
-    switch (dbClient) {
-        case 'pg':
-            dbClientPackage = pg;
-            break;
-        default:
-            throw new Error(`Unsupported database client: ${dbClient}`);
-    }
+  let knex = dbClientPackage(knexConfig);
 
-    let knex = dbClientPackage(knexConfig);
+  // Check if Database Exists & create if it doesn't
+  let hasDatabase: any;
+  switch (dbClient) {
+    case "pg":
+      hasDatabase = await knex.raw(
+        `SELECT 1 from pg_database WHERE datname = '${DATABASE_NAME}'`,
+      );
+      server.log.info(`CHECKING for Database ${DATABASE_NAME}...`);
+      if (!hasDatabase.rows.length) {
+        await knex.raw(`CREATE DATABASE ${DATABASE_NAME}`);
+      } else {
+        server.log.info(`Database ${DATABASE_NAME} already exists`);
+      }
+      break;
+    default:
+      throw new Error(
+        `Unsupported database client: ${dbClient}. Cannot create database ${DATABASE_NAME}`,
+      );
+  }
 
-    // Check if Database Exists & create if it doesn't
-    let hasDatabase: any;
-    switch (dbClient) {
-        case 'pg':
-            hasDatabase = await knex.raw(`SELECT 1 from pg_database WHERE datname = '${DATABASE_NAME}'`)
-            server.log.info(`CHECKING for Database ${DATABASE_NAME}...`);
-            if (!hasDatabase.rows.length) {
-                await knex.raw(`CREATE DATABASE ${DATABASE_NAME}`);
-            } else {
-                server.log.info(`Database ${DATABASE_NAME} already exists`);
-            }
-            break;
-        default:
-            throw new Error(`Unsupported database client: ${dbClient}. Cannot create database ${DATABASE_NAME}`);
-    }
+  // Updated the DATABASE name on connection object
+  connection.database = DATABASE_NAME;
 
-    // Updated the DATABASE name on connection object
-    connection.database = DATABASE_NAME;
+  // Updating knex Config
+  knexConfig = {
+    client: dbClient,
+    connection,
+  };
 
-    // Updating knex Config
-    knexConfig = {
-        client: dbClient,
-        connection,
-    };
+  // re-instantiate connection with new config
+  knex = dbClientPackage(knexConfig);
 
-    // re-instantiate connection with new config
-    knex = dbClientPackage(knexConfig);
+  return knex;
+};
 
-    return knex;
-}
+export const connectWithDatabase = async (
+  server: FastifyInstance | FastifyRequest,
+): Promise<Knex> => {
+  // Creating KNEX Config
+  let connection: Knex.ConnectionConfig = {
+    host: getEnv("POSTGRES_HOST"),
+    user: getEnv("POSTGRES_USER"),
+    password: getEnv("POSTGRES_PASSWORD"),
+    database: DATABASE_NAME,
+  };
 
+  let knexConfig: Knex.Config = {
+    client: dbClient,
+    connection,
+  };
 
-export const connectWithDatabase = async (server: FastifyInstance | FastifyRequest) : Promise<Knex> => {
+  // Set the appropriate databse client package
+  let dbClientPackage: any;
+  switch (dbClient) {
+    case "pg":
+      dbClientPackage = pg;
+      break;
+    default:
+      throw new Error(`Unsupported database client: ${dbClient}`);
+  }
 
-    // Creating KNEX Config
-    let connection: Knex.ConnectionConfig = {
-        host: getEnv('POSTGRES_HOST'),
-        user: getEnv('POSTGRES_USER'),
-        password: getEnv('POSTGRES_PASSWORD'),
-        database: DATABASE_NAME
-    };
+  // instantiate connection with new config
+  const knex = dbClientPackage(knexConfig);
 
-    let knexConfig: Knex.Config = {
-        client: dbClient,
-        connection,
-    };
-
-    // Set the appropriate databse client package
-    let dbClientPackage: any;
-    switch (dbClient) {
-        case 'pg':
-            dbClientPackage = pg;
-            break;
-        default:
-            throw new Error(`Unsupported database client: ${dbClient}`);
-    }
-
-    // instantiate connection with new config
-    const knex = dbClientPackage(knexConfig);
-
-    return knex;
-}
+  return knex;
+};
