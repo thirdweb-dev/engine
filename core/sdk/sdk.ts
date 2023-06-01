7484a0bada69b0d69964cc350ae1ac94c8398d37
Joaquim Verges <joaquim.verges@gmail.com>
2023-06-01 11:40:10 -0700

    add /deployer/contractTypes endpoint (#40)
    * add /deployer/contractTypes endpoint
    
    * cleanup sdk file

--- core/sdk/sdk.ts
+++ core/sdk/sdk.ts
@@ -1,6 +1,5 @@
 import { ThirdwebSDK, ChainOrRpc } from "@thirdweb-dev/sdk";
 import { getEnv } from "../loadEnv";
-// import { AwsKmsWallet } from '@thirdweb-dev/sdk/evm/wallets';
 
 // Cache the SDK in memory so it doesn't get reinstantiated unless the server crashes
 // This saves us from making a request to get the private key for reinstantiation on every request
@@ -11,27 +10,11 @@ export async function getSDK(chainName: ChainOrRpc): Promise<ThirdwebSDK> {
     return sdkMap[chainName] as ThirdwebSDK;
   }
 
-  // Need to make this instantiate SDK with read/write. For that will need wallet information
-  // Currently only doing read-only mode as per Docs
-  // if (getEnv('NODE_ENV') === 'local'){
-  sdkMap[chainName] = await ThirdwebSDK.fromPrivateKey(
+  sdkMap[chainName] = ThirdwebSDK.fromPrivateKey(
     getEnv("WALLET_PRIVATE_KEY"),
     chainName,
     { thirdwebApiKey: getEnv("THIRDWEB_API_KEY") },
   );
-  // }
-  // else if (getEnv('USE_WALLET') === 'true') {
-  //   const wallet = new AwsKmsWallet({
-  //     region: getEnv('AWS_REGION') as string,
-  //     accessKeyId: getEnv('AWS_ACCESS_KEY_ID') as string,
-  //     secretAccessKey: getEnv('AWS_SECRET_ACCESS_KEY') as string,
-  //     keyId: getEnv('AWS_KMS_KEY_ID') as string,
-  //   });
-  //   sdkMap[chainName] = await ThirdwebSDK.fromWallet(wallet, chainName);
-  // }
-  // else {
-  //   sdkMap[chainName] = new ThirdwebSDK(chainName);
-  // }
 
   return sdkMap[chainName] as ThirdwebSDK;
 }
--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from "fastify";
 import { readContract } from "./contract/read/read";
 import { writeToContract } from "./contract/write/write";
 
-import { checkTxStatus } from './transaction/status';
+import { checkTxStatus } from "./transaction/status";
 
 // Extensions
 import { erc20Routes } from "./contract/extensions/erc20/index";
@@ -10,11 +10,13 @@ import { deployPrebuilt } from "./deployer/prebuilt";
 import { deployPublished } from "./deployer/published";
 import { erc721Routes } from "./contract/extensions/erc721";
 import { erc1155Routes } from "./contract/extensions/erc1155";
+import { contractTypes } from "./deployer/contractTypes";
 
 export const apiRoutes = async (fastify: FastifyInstance) => {
   // deployer
   await fastify.register(deployPrebuilt);
   await fastify.register(deployPublished);
+  await fastify.register(contractTypes);
 
   // generic
   await fastify.register(readContract);
