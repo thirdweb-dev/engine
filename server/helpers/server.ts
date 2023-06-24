55f3c86555be5a3dac30a0d925378280e397415d
Farhan Khwaja <farhan@thirdweb.com>
2023-06-23 17:46:16 -0700

    modularized server creation for testing

---
+++ server/helpers/server.ts
@@ -0,0 +1,92 @@
+import fastifyCors from "@fastify/cors";
+import fastifyExpress from "@fastify/express";
+import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+import { getLogSettings, errorHandler, getEnv } from "../../core";
+import fastify, { FastifyInstance } from "fastify";
+import { apiRoutes } from "../../server/api";
+import { openapi } from "./openapi";
+import * as fs from "fs";
+
+const createServer = async (): Promise<FastifyInstance> => {
+  const logOptions = getLogSettings("API-Server");
+
+  const server: FastifyInstance = fastify({
+    logger: logOptions ?? true,
+    disableRequestLogging: true,
+  }).withTypeProvider<TypeBoxTypeProvider>();
+
+  server.addHook("preHandler", function (request, reply, done) {
+    if (
+      !request.routerPath?.includes("static") &&
+      !request.routerPath?.includes("json")
+    ) {
+      if (request.body && Object.keys(request.body).length > 0) {
+        request.log.info({ ...request.body }, "Request Body : ");
+      }
+
+      if (request.params && Object.keys(request.params).length > 0) {
+        request.log.info({ ...request.params }, "Request Params : ");
+      }
+
+      if (request.query && Object.keys(request.query).length > 0) {
+        request.log.info({ ...request.query }, "Request Querystring : ");
+      }
+    }
+
+    done();
+  });
+
+  server.addHook("onRequest", (request, reply, done) => {
+    if (
+      !request.routerPath?.includes("static") &&
+      !request.routerPath?.includes("json")
+    ) {
+      request.log.info(
+        `Request received - ${request.method} - ${request.routerPath}`,
+      );
+    }
+    done();
+  });
+
+  server.addHook("onResponse", (request, reply, done) => {
+    if (
+      !request.routerPath?.includes("static") &&
+      !request.routerPath?.includes("json")
+    ) {
+      request.log.info(
+        `Request completed - ${request.method} - ${
+          reply.request.routerPath
+        } - StatusCode: ${reply.statusCode} - Response Time: ${reply
+          .getResponseTime()
+          .toFixed(2)}ms`,
+      );
+    }
+    done();
+  });
+
+  await errorHandler(server);
+
+  await server.register(fastifyCors);
+
+  await server.register(fastifyExpress);
+
+  openapi(server);
+
+  await server.register(apiRoutes);
+
+  await server.ready();
+
+  // Command to Generate Swagger File
+  // Needs to be called post Fastify Server is Ready
+  server.swagger();
+
+  // To Generate Swagger YAML File
+  if (getEnv("NODE_ENV") === "local") {
+    const yaml = server.swagger({ yaml: true });
+    fs.writeFileSync("./swagger.yml", yaml);
+  }
+
+  return server;
+};
+
+export default createServer;
--- server/index.ts
+++ server/index.ts
@@ -1,95 +1,9 @@
-import fastify, { FastifyInstance } from "fastify";
-import fastifyExpress from "@fastify/express";
-import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
-import * as fs from "fs";
-import fastifyCors from "@fastify/cors";
-import { openapi } from "./helpers";
-import { errorHandler, getEnv } from "../core";
-import { apiRoutes } from "./api";
-import {
-  checkTablesExistence,
-  implementTriggerOnStartUp,
-  getLogSettings,
-} from "../core";
+import { getEnv } from "../core";
+import createServer from "./helpers/server";
+import { checkTablesExistence, implementTriggerOnStartUp } from "../core";
 
 const main = async () => {
-  const logOptions = getLogSettings("API-Server");
-
-  const server: FastifyInstance = fastify({
-    logger: logOptions ?? true,
-    disableRequestLogging: true,
-  }).withTypeProvider<TypeBoxTypeProvider>();
-
-  server.addHook("preHandler", function (request, reply, done) {
-    if (
-      !request.routerPath?.includes("static") &&
-      !request.routerPath?.includes("json")
-    ) {
-      if (request.body && Object.keys(request.body).length > 0) {
-        request.log.info({ ...request.body }, "Request Body : ");
-      }
-
-      if (request.params && Object.keys(request.params).length > 0) {
-        request.log.info({ ...request.params }, "Request Params : ");
-      }
-
-      if (request.query && Object.keys(request.query).length > 0) {
-        request.log.info({ ...request.query }, "Request Querystring : ");
-      }
-    }
-
-    done();
-  });
-
-  server.addHook("onRequest", (request, reply, done) => {
-    if (
-      !request.routerPath?.includes("static") &&
-      !request.routerPath?.includes("json")
-    ) {
-      request.log.info(
-        `Request received - ${request.method} - ${request.routerPath}`,
-      );
-    }
-    done();
-  });
-
-  server.addHook("onResponse", (request, reply, done) => {
-    if (
-      !request.routerPath?.includes("static") &&
-      !request.routerPath?.includes("json")
-    ) {
-      request.log.info(
-        `Request completed - ${request.method} - ${
-          reply.request.routerPath
-        } - StatusCode: ${reply.statusCode} - Response Time: ${reply
-          .getResponseTime()
-          .toFixed(2)}ms`,
-      );
-    }
-    done();
-  });
-
-  await errorHandler(server);
-
-  await server.register(fastifyCors);
-
-  await server.register(fastifyExpress);
-
-  openapi(server);
-
-  await server.register(apiRoutes);
-
-  await server.ready();
-
-  // Command to Generate Swagger File
-  // Needs to be called post Fastify Server is Ready
-  server.swagger();
-
-  // To Generate Swagger YAML File
-  if (getEnv("NODE_ENV") === "local") {
-    const yaml = server.swagger({ yaml: true });
-    fs.writeFileSync("./swagger.yml", yaml);
-  }
+  const server = await createServer();
 
   server.listen(
     {
