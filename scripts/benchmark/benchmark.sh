bf2a0d8f89192621faf35058c0a2eacfc87b8406
farhanW3 <farhan@thirdweb.com>
2023-08-03 12:33:34 -0700

    updated benchmark script

--- scripts/benchmark/benchmark.sh
+++ scripts/benchmark/benchmark.sh
@@ -10,6 +10,6 @@ echo $BENCHMARK_POST_BODY > post_data.json
 
 /usr/bin/ab -p ./post_data.json \
   -T 'application/json' \
-  -H 'x-secret-key:fa1a21a6d073902986843e45458acf47f721caed91765623e3951a36ae5c8f3b30173c6446087c3cc82f516ee7ce5d115c4e9ed87bc6d1842f5e95349d771a84' \
+  -H 'x-secret-key:<copy_your_tw_secret_key_from_env_file>' \
   -c $BENCHMARK_CONCURRENCY -n $BENCHMARK_REQUESTS \
   "$BENCHMARK_HOST$BENCHMARK_URL_PATH"
