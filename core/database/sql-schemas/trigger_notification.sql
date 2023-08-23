d59a0df998e295b0ae103e1fc94ef08f1268b4d4
farhanW3 <farhan@thirdweb.com>
2023-08-22 18:14:15 -0700

    removed commented triggers, types folder for fastify

--- core/database/sql-schemas/trigger_notification.sql
+++ core/database/sql-schemas/trigger_notification.sql
@@ -8,19 +8,7 @@ BEGIN
 END;
 $function$;
 
--- Original Trigger returns all data
--- CREATE OR REPLACE FUNCTION notify_transactions_update()
---   RETURNS TRIGGER
---   LANGUAGE plpgsql
--- AS $function$
--- BEGIN
---   PERFORM pg_notify('updated_transaction_data', row_to_json(NEW)::text);
---   RETURN NEW;
--- END;
--- $function$;
-
-
--- New Trigger to return only specific data, to keep it under the size limit
+-- Trigger to return only specific column, to keep it under the size limit
 CREATE OR REPLACE FUNCTION notify_transactions_update()
   RETURNS TRIGGER
   LANGUAGE plpgsql
@@ -32,28 +20,3 @@ BEGIN
   RETURN NEW;
 END;
 $function$;
-
--- New Trigger to check for payload size and return appropriate data
--- CREATE OR REPLACE FUNCTION notify_transactions_update()
---   RETURNS TRIGGER
---   LANGUAGE plpgsql
--- AS $function$
--- DECLARE
---     payload TEXT;
--- BEGIN
---     payload := row_to_json(NEW)::text;
-
---     IF length(payload) < 8000 THEN
---         PERFORM pg_notify('updated_transaction_data', payload);
---     ELSE
---         payload := json_build_object(
---           'identifier', NEW.identifier,
---           'txErrored', NEW.txErrored,
---           'errorMessage', NEW.errorMessage
---         )::text;
---         PERFORM pg_notify('updated_transaction_data', payload);
---     END IF;
-
---     RETURN NEW;
--- END;
--- $function$;
