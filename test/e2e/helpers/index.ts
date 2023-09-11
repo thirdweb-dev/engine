7efd90cde0d857e42d4ed9c5c1b9d83ea0254a52
Adam Majmudar <64697628+adam-maj@users.noreply.github.com>
2023-09-11 16:06:54 -0700

    Organize testing & benchmarking and add default setup (#113)
    * Replace benchmarking
    
    * Add defaults to benchmark script

--- test/e2e/helpers/index.ts
+++ test/e2e/helpers/index.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from "fastify";
 import request from "supertest";
-import { env } from "../../core";
-import { TransactionStatusEnum } from "../../server/schemas/transaction";
+import { env } from "../../../core";
+import { TransactionStatusEnum } from "../../../server/schemas/transaction";
 
 export const awaitTransactionSubmission = async (
   server: FastifyInstance,
--- test/e2e/setup-init.ts
+++ test/e2e/setup-init.ts
@@ -1,5 +1,5 @@
-import { checkTablesExistence, implementTriggerOnStartUp } from "../core";
-import createServer from "../server/helpers/server";
+import { checkTablesExistence, implementTriggerOnStartUp } from "../../core";
+import createServer from "../../server/helpers/server";
 
 const server = await createServer("Test-Suite");
 server.log.info("Setting up required Tables, Triggers and Functions");
