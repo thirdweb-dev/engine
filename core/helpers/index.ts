0af5faa62379283b2a797da868ca99a5bf2387bf
Adam Majmudar <mr.adam.maj@gmail.com>
2023-09-13 16:03:01 -0700

    Remove wallet type bug

--- core/helpers/index.ts
+++ core/helpers/index.ts
@@ -17,27 +17,3 @@ export const isValidHttpUrl = (urlString: string): boolean => {
 
   return url.protocol === "http:" || url.protocol === "https:";
 };
-
-export const getWalletType = (): string => {
-  if (WALLET_PRIVATE_KEY) {
-    return "ppk";
-  }
-
-  if (AWS_ACCESS_KEY_ID && AWS_SECRET_ACCESS_KEY && AWS_REGION) {
-    return "aws_kms";
-  }
-
-  if (
-    env.GOOGLE_APPLICATION_CREDENTIAL_EMAIL &&
-    env.GOOGLE_APPLICATION_CREDENTIAL_PRIVATE_KEY &&
-    env.GOOGLE_APPLICATION_PROJECT_ID &&
-    env.GOOGLE_KMS_KEY_RING_ID &&
-    env.GOOGLE_KMS_KEY_VERSION_ID &&
-    env.GOOGLE_KMS_LOCATION_ID
-  ) {
-    return "aws_kms";
-  }
-
-  // ToDo GCP KMS
-  return "local";
-};
