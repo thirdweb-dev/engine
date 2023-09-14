41c5d26a897865b5030d863d7107ac5c432de01c
Adam Majmudar <mr.adam.maj@gmail.com>
2023-09-14 11:56:07 -0700

    Update to make transaction processing a prisma.

--- worker/controller/processTransaction.ts
+++ worker/controller/processTransaction.ts
@@ -4,6 +4,7 @@ import { FastifyInstance } from "fastify";
 import { StatusCodes } from "http-status-codes";
 import { createCustomError, env, getSDK } from "../../core";
 import { TransactionStatusEnum } from "../../server/schemas/transaction";
+import { prisma } from "../../src/db/client";
 import { getQueuedTxs } from "../../src/db/transactions/getQueuedTxs";
 import { updateTx } from "../../src/db/transactions/updateTx";
 import { getWalletDetails } from "../../src/db/wallets/getWalletDetails";
@@ -16,117 +17,118 @@ export const processTransaction = async (
 ): Promise<string[]> => {
   let processedIds: string[] = [];
   try {
-    // Connect to the DB
-    let data;
-    try {
-      data = await getQueuedTxs();
-    } catch (error) {
-      const customError = createCustomError(
-        "Error in getting transactions from table",
-        StatusCodes.INTERNAL_SERVER_ERROR,
-        "TRANSACTION_PROCESSING_ERROR",
-      );
-      throw customError;
-    }
-
-    if (data.length < MIN_TRANSACTION_TO_PROCESS) {
-      server.log.warn(
-        `Number of transactions to process less than Minimum Transactions to Process: ${MIN_TRANSACTION_TO_PROCESS}`,
-      );
-      server.log.warn(
-        `Waiting for more transactions requests to start processing`,
-      );
-      return [];
-    }
-
-    processedIds = data.map((row: any) => row.identifier);
-    for (const tx of data) {
-      server.log.info(`Processing Transaction: ${tx.queueId}`);
-      const walletDetails = await getWalletDetails({
-        address: tx.fromAddress!,
-        chainId: tx.chainId!,
-      });
-      const sdk = await getSDK(tx.chainId!.toString(), tx.fromAddress!);
-
-      let [blockchainNonce, gasData, currentBlockNumber] = await Promise.all([
-        sdk.wallet.getNonce("pending"),
-        getDefaultGasOverrides(sdk.getProvider()),
-        sdk.getProvider().getBlockNumber(),
-      ]);
-
-      // TODO: IMPORTANT: Proper nonce management logic! Add comments!
-      let currentNonce = BigNumber.from(walletDetails?.nonce ?? 0);
-      let txSubmittedNonce = BigNumber.from(0);
-
-      if (BigNumber.from(blockchainNonce).gt(currentNonce)) {
-        txSubmittedNonce = BigNumber.from(blockchainNonce);
-      } else {
-        txSubmittedNonce = BigNumber.from(currentNonce);
+    await prisma.$transaction(async () => {
+      let data;
+      try {
+        data = await getQueuedTxs();
+      } catch (error) {
+        const customError = createCustomError(
+          "Error in getting transactions from table",
+          StatusCodes.INTERNAL_SERVER_ERROR,
+          "TRANSACTION_PROCESSING_ERROR",
+        );
+        throw customError;
       }
 
-      await updateTx({
-        queueId: tx.queueId!,
-        status: TransactionStatusEnum.Processed,
-      });
-
-      // Get the nonce for the blockchain transaction
-
-      // Submit transaction to the blockchain
-      // Create transaction object
-      const txObject: providers.TransactionRequest = {
-        to: tx.toAddress!,
-        from: tx.fromAddress!,
-        data: tx.data!,
-        nonce: txSubmittedNonce,
-        value: tx.value!,
-        ...gasData,
-      };
-
-      // Send transaction to the blockchain
-      let txRes: ethers.providers.TransactionResponse | undefined;
-      try {
-        txRes = await sdk.getSigner()?.sendTransaction(txObject);
-      } catch (error: any) {
-        server.log.debug("Send Transaction errored");
+      if (data.length < MIN_TRANSACTION_TO_PROCESS) {
         server.log.warn(
-          `Request-ID: ${tx.queueId} processed but errored out: Commited to db`,
+          `Number of transactions to process less than Minimum Transactions to Process: ${MIN_TRANSACTION_TO_PROCESS}`,
         );
-
-        await updateTx({
-          queueId: tx.queueId!,
-          status: TransactionStatusEnum.Errored,
-          txData: {
-            errorMessage: error.message,
-          },
-        });
-        throw error;
+        server.log.warn(
+          `Waiting for more transactions requests to start processing`,
+        );
+        return [];
       }
 
-      try {
-        await updateTx({
-          queueId: tx.queueId!,
-          status: TransactionStatusEnum.Submitted,
-          res: txRes,
-        });
-        server.log.info(
-          `Transaction submitted for ${tx.queueId!} with Nonce ${txSubmittedNonce}, Tx Hash: ${
-            txRes?.hash
-          } `,
-        );
-
-        await updateWalletNonce({
+      processedIds = data.map((row: any) => row.identifier);
+      for (const tx of data) {
+        server.log.info(`Processing Transaction: ${tx.queueId}`);
+        const walletDetails = await getWalletDetails({
           address: tx.fromAddress!,
           chainId: tx.chainId!,
-          // TODO: IMPORTANT: This will cause errors!
-          // TODO: Should this be txSubmittedNonce or blockchainNonce?
-          nonce: txSubmittedNonce.toNumber() + 1,
         });
-      } catch (error) {
-        server.log.warn("Transaction failed with error:");
-        server.log.error(error);
-        throw error;
+        const sdk = await getSDK(tx.chainId!.toString(), tx.fromAddress!);
+
+        let [blockchainNonce, gasData, currentBlockNumber] = await Promise.all([
+          sdk.wallet.getNonce("pending"),
+          getDefaultGasOverrides(sdk.getProvider()),
+          sdk.getProvider().getBlockNumber(),
+        ]);
+
+        // TODO: IMPORTANT: Proper nonce management logic! Add comments!
+        let currentNonce = BigNumber.from(walletDetails?.nonce ?? 0);
+        let txSubmittedNonce = BigNumber.from(0);
+
+        if (BigNumber.from(blockchainNonce).gt(currentNonce)) {
+          txSubmittedNonce = BigNumber.from(blockchainNonce);
+        } else {
+          txSubmittedNonce = BigNumber.from(currentNonce);
+        }
+
+        await updateTx({
+          queueId: tx.queueId!,
+          status: TransactionStatusEnum.Processed,
+        });
+
+        // Get the nonce for the blockchain transaction
+
+        // Submit transaction to the blockchain
+        // Create transaction object
+        const txObject: providers.TransactionRequest = {
+          to: tx.toAddress!,
+          from: tx.fromAddress!,
+          data: tx.data!,
+          nonce: txSubmittedNonce,
+          value: tx.value!,
+          ...gasData,
+        };
+
+        // Send transaction to the blockchain
+        let txRes: ethers.providers.TransactionResponse | undefined;
+        try {
+          txRes = await sdk.getSigner()?.sendTransaction(txObject);
+        } catch (error: any) {
+          server.log.debug("Send Transaction errored");
+          server.log.warn(
+            `Request-ID: ${tx.queueId} processed but errored out: Commited to db`,
+          );
+
+          await updateTx({
+            queueId: tx.queueId!,
+            status: TransactionStatusEnum.Errored,
+            txData: {
+              errorMessage: error.message,
+            },
+          });
+          throw error;
+        }
+
+        try {
+          await updateTx({
+            queueId: tx.queueId!,
+            status: TransactionStatusEnum.Submitted,
+            res: txRes,
+          });
+          server.log.info(
+            `Transaction submitted for ${tx.queueId!} with Nonce ${txSubmittedNonce}, Tx Hash: ${
+              txRes?.hash
+            } `,
+          );
+
+          await updateWalletNonce({
+            address: tx.fromAddress!,
+            chainId: tx.chainId!,
+            // TODO: IMPORTANT: This will cause errors!
+            // TODO: Should this be txSubmittedNonce or blockchainNonce?
+            nonce: txSubmittedNonce.toNumber() + 1,
+          });
+        } catch (error) {
+          server.log.warn("Transaction failed with error:");
+          server.log.error(error);
+          throw error;
+        }
       }
-    }
+    });
   } catch (error) {
     server.log.error(error);
   } finally {
--- worker/controller/retryTransaction.ts
+++ worker/controller/retryTransaction.ts
@@ -3,6 +3,7 @@ import { BigNumber, ethers, providers } from "ethers";
 import { FastifyInstance } from "fastify";
 import { env, getSDK } from "../../core";
 import { TransactionStatusEnum } from "../../server/schemas/transaction";
+import { prisma } from "../../src/db/client";
 import { getTxToRetry } from "../../src/db/transactions/getTxToRetry";
 import { updateTx } from "../../src/db/transactions/updateTx";
 import { getTransactionReceiptWithBlockDetails } from "../services/blockchain";
@@ -19,137 +20,140 @@ export const retryTransactions = async (server: FastifyInstance) => {
       server.log.warn("Retry Tx Cron is disabled");
       return;
     }
-    server.log.info("Running Cron to Retry transactions on blockchain");
-    const transactions = await getTxToRetry();
 
-    if (transactions.length === 0) {
-      server.log.warn("No transactions to retry");
-      return;
-    }
+    await prisma.$transaction(async () => {
+      server.log.info("Running Cron to Retry transactions on blockchain");
+      const transactions = await getTxToRetry();
 
-    const txReceiptsWithChainId = await getTransactionReceiptWithBlockDetails(
-      server,
-      transactions,
-    );
+      if (transactions.length === 0) {
+        server.log.warn("No transactions to retry");
+        return;
+      }
 
-    for (let txReceiptData of txReceiptsWithChainId) {
-      // Check if transaction got mined on chain
-      if (
-        txReceiptData.blockNumber != -1 &&
-        txReceiptData.chainId &&
-        txReceiptData.queueId &&
-        txReceiptData.txHash != "" &&
-        txReceiptData.effectiveGasPrice != BigNumber.from(-1) &&
-        txReceiptData.timestamp != -1
-      ) {
-        server.log.debug(
-          `Got receipt for tx: ${txReceiptData.txHash}, queueId: ${txReceiptData.queueId}, effectiveGasPrice: ${txReceiptData.effectiveGasPrice}`,
-        );
-      } else {
-        const sdk = await getSDK(
-          txReceiptData.txData.chainId!.toString(),
-          txReceiptData.txData.fromAddress!,
-        );
-        const currentBlockNumber = await sdk.getProvider().getBlockNumber();
+      const txReceiptsWithChainId = await getTransactionReceiptWithBlockDetails(
+        server,
+        transactions,
+      );
 
+      for (let txReceiptData of txReceiptsWithChainId) {
+        // Check if transaction got mined on chain
         if (
-          currentBlockNumber - txReceiptData.txData.sentAtBlockNumber! >
-          env.MAX_BLOCKS_ELAPSED_BEFORE_RETRY
+          txReceiptData.blockNumber != -1 &&
+          txReceiptData.chainId &&
+          txReceiptData.queueId &&
+          txReceiptData.txHash != "" &&
+          txReceiptData.effectiveGasPrice != BigNumber.from(-1) &&
+          txReceiptData.timestamp != -1
         ) {
           server.log.debug(
-            `Retrying tx: ${txReceiptData.txHash}, queueId: ${txReceiptData.queueId} with higher gas values`,
-          );
-
-          const gasData = await getDefaultGasOverrides(sdk.getProvider());
-          server.log.debug(
-            `Gas Data MaxFeePerGas: ${gasData.maxFeePerGas}, MaxPriorityFeePerGas: ${gasData.maxPriorityFeePerGas}, gasPrice`,
-          );
-
-          if (
-            gasData.maxFeePerGas?.lte(
-              BigNumber.from(txReceiptData.txData.maxFeePerGas),
-            )
-          ) {
-            server.log.warn(
-              `Gas Data MaxFeePerGas: ${gasData.maxFeePerGas} is less than or equal to Previously Submitted Transaction: ${txReceiptData.txData.maxFeePerGas} for queueId: ${txReceiptData.queueId}. Will Retry Later`,
-            );
-            continue;
-          }
-
-          // Re-Submit transaction to the blockchain
-          // Create transaction object
-          const txObject: providers.TransactionRequest = {
-            to: txReceiptData.txData.toAddress!,
-            from: txReceiptData.txData.fromAddress!,
-            data: txReceiptData.txData.data!,
-            nonce: txReceiptData.txData.nonce!,
-            value: txReceiptData.txData.value!,
-            ...gasData,
-          };
-
-          // Override gas values from DB if flag is true
-          if (txReceiptData.txData.retryGasValues) {
-            server.log.info(
-              `Setting Gas Values from DB as override flag is set to true. MaxFeePerGas: ${txReceiptData.txData.retryMaxFeePerGas}, MaxPriorityFeePerGas: ${txReceiptData.txData.retryMaxPriorityFeePerGas} for queueId: ${txReceiptData.queueId}`,
-            );
-            txObject.maxFeePerGas = txReceiptData.txData.retryMaxFeePerGas!;
-            txObject.maxPriorityFeePerGas =
-              txReceiptData.txData.retryMaxPriorityFeePerGas!;
-          } else if (
-            gasData.maxFeePerGas?.gt(MAX_FEE_PER_GAS_FOR_RETRY!) ||
-            gasData.maxPriorityFeePerGas?.gt(
-              MAX_PRIORITY_FEE_PER_GAS_FOR_RETRY!,
-            )
-          ) {
-            server.log.warn(
-              `${txReceiptData.txData
-                .chainId!} Chain Gas Price is higher than Max Threshold for retrying transaction ${
-                txReceiptData.queueId
-              }. Will try again after ${
-                env.MAX_BLOCKS_ELAPSED_BEFORE_RETRY
-              } blocks or in 30 seconds`,
-            );
-            continue;
-          }
-
-          // Send transaction to the blockchain
-          let txRes: ethers.providers.TransactionResponse | undefined;
-          try {
-            txRes = await sdk.getSigner()?.sendTransaction(txObject);
-          } catch (error: any) {
-            server.log.debug("Send Transaction errored");
-            server.log.warn(
-              `Request-ID: ${txReceiptData.queueId} processed but errored out: Commited to db`,
-            );
-            throw error;
-          }
-
-          await updateTx({
-            queueId: txReceiptData.txData.queueId!,
-            status: TransactionStatusEnum.Submitted,
-            res: txRes,
-            txData: {
-              retryCount: txReceiptData.txData.retryCount + 1,
-            },
-          });
-          server.log.info(
-            `Transaction re-submitted for ${txReceiptData.queueId} with Nonce ${txReceiptData.txData.nonce}, Tx Hash: ${txRes?.hash}`,
+            `Got receipt for tx: ${txReceiptData.txHash}, queueId: ${txReceiptData.queueId}, effectiveGasPrice: ${txReceiptData.effectiveGasPrice}`,
           );
         } else {
-          server.log.info(
-            `Will retry later Request with queueId ${
-              txReceiptData.queueId
-            } after ${
-              env.MAX_BLOCKS_ELAPSED_BEFORE_RETRY
-            } blocks, Elasped Blocks: ${
-              currentBlockNumber - txReceiptData.txData.sentAtBlockNumber!
-            }`,
+          const sdk = await getSDK(
+            txReceiptData.txData.chainId!.toString(),
+            txReceiptData.txData.fromAddress!,
           );
+          const currentBlockNumber = await sdk.getProvider().getBlockNumber();
+
+          if (
+            currentBlockNumber - txReceiptData.txData.sentAtBlockNumber! >
+            env.MAX_BLOCKS_ELAPSED_BEFORE_RETRY
+          ) {
+            server.log.debug(
+              `Retrying tx: ${txReceiptData.txHash}, queueId: ${txReceiptData.queueId} with higher gas values`,
+            );
+
+            const gasData = await getDefaultGasOverrides(sdk.getProvider());
+            server.log.debug(
+              `Gas Data MaxFeePerGas: ${gasData.maxFeePerGas}, MaxPriorityFeePerGas: ${gasData.maxPriorityFeePerGas}, gasPrice`,
+            );
+
+            if (
+              gasData.maxFeePerGas?.lte(
+                BigNumber.from(txReceiptData.txData.maxFeePerGas),
+              )
+            ) {
+              server.log.warn(
+                `Gas Data MaxFeePerGas: ${gasData.maxFeePerGas} is less than or equal to Previously Submitted Transaction: ${txReceiptData.txData.maxFeePerGas} for queueId: ${txReceiptData.queueId}. Will Retry Later`,
+              );
+              continue;
+            }
+
+            // Re-Submit transaction to the blockchain
+            // Create transaction object
+            const txObject: providers.TransactionRequest = {
+              to: txReceiptData.txData.toAddress!,
+              from: txReceiptData.txData.fromAddress!,
+              data: txReceiptData.txData.data!,
+              nonce: txReceiptData.txData.nonce!,
+              value: txReceiptData.txData.value!,
+              ...gasData,
+            };
+
+            // Override gas values from DB if flag is true
+            if (txReceiptData.txData.retryGasValues) {
+              server.log.info(
+                `Setting Gas Values from DB as override flag is set to true. MaxFeePerGas: ${txReceiptData.txData.retryMaxFeePerGas}, MaxPriorityFeePerGas: ${txReceiptData.txData.retryMaxPriorityFeePerGas} for queueId: ${txReceiptData.queueId}`,
+              );
+              txObject.maxFeePerGas = txReceiptData.txData.retryMaxFeePerGas!;
+              txObject.maxPriorityFeePerGas =
+                txReceiptData.txData.retryMaxPriorityFeePerGas!;
+            } else if (
+              gasData.maxFeePerGas?.gt(MAX_FEE_PER_GAS_FOR_RETRY!) ||
+              gasData.maxPriorityFeePerGas?.gt(
+                MAX_PRIORITY_FEE_PER_GAS_FOR_RETRY!,
+              )
+            ) {
+              server.log.warn(
+                `${txReceiptData.txData
+                  .chainId!} Chain Gas Price is higher than Max Threshold for retrying transaction ${
+                  txReceiptData.queueId
+                }. Will try again after ${
+                  env.MAX_BLOCKS_ELAPSED_BEFORE_RETRY
+                } blocks or in 30 seconds`,
+              );
+              continue;
+            }
+
+            // Send transaction to the blockchain
+            let txRes: ethers.providers.TransactionResponse | undefined;
+            try {
+              txRes = await sdk.getSigner()?.sendTransaction(txObject);
+            } catch (error: any) {
+              server.log.debug("Send Transaction errored");
+              server.log.warn(
+                `Request-ID: ${txReceiptData.queueId} processed but errored out: Commited to db`,
+              );
+              throw error;
+            }
+
+            await updateTx({
+              queueId: txReceiptData.txData.queueId!,
+              status: TransactionStatusEnum.Submitted,
+              res: txRes,
+              txData: {
+                retryCount: txReceiptData.txData.retryCount + 1,
+              },
+            });
+            server.log.info(
+              `Transaction re-submitted for ${txReceiptData.queueId} with Nonce ${txReceiptData.txData.nonce}, Tx Hash: ${txRes?.hash}`,
+            );
+          } else {
+            server.log.info(
+              `Will retry later Request with queueId ${
+                txReceiptData.queueId
+              } after ${
+                env.MAX_BLOCKS_ELAPSED_BEFORE_RETRY
+              } blocks, Elasped Blocks: ${
+                currentBlockNumber - txReceiptData.txData.sentAtBlockNumber!
+              }`,
+            );
+          }
         }
       }
-    }
 
-    return;
+      return;
+    });
   } catch (error) {
     server.log.error(error);
     return;
