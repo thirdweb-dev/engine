0a5beb7a16a2a51a3f991d160537859e771da98c
Adam Majmudar <mr.adam.maj@gmail.com>
2023-09-14 17:37:14 -0700

    Remove copy files and fix dockerfile

--- core/database/sql-schemas/trigger_tx_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE TRIGGER transactions_insert_trigger
-  AFTER INSERT ON transactions
-  FOR EACH ROW
-  EXECUTE FUNCTION notify_transactions_insert();
-
-CREATE OR REPLACE TRIGGER transactions_update_trigger
-  AFTER UPDATE ON transactions
-  FOR EACH ROW
-  EXECUTE FUNCTION notify_transactions_update();
