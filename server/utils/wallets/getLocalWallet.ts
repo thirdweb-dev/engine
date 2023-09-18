2339f7c819bfaffc52ce6d56cba53166f5f00b63
Adam Majmudar <mr.adam.maj@gmail.com>
2023-09-18 16:56:53 -0700

    Pass chain for local wallet

--- server/utils/wallets/getLocalWallet.ts
+++ server/utils/wallets/getLocalWallet.ts
@@ -1,15 +1,18 @@
+import { Chain } from "@thirdweb-dev/chains";
 import { LocalWallet } from "@thirdweb-dev/wallets";
 import { LocalFileStorage } from "../../../core";
 import { env } from "../../../core/env";
 
 interface GetLocalWalletParams {
+  chain: Chain;
   walletAddress: string;
 }
 
 export const getLocalWallet = async ({
+  chain,
   walletAddress,
 }: GetLocalWalletParams) => {
-  const wallet = new LocalWallet();
+  const wallet = new LocalWallet({ chain });
   await wallet.load({
     strategy: "encryptedJson",
     password: env.THIRDWEB_API_SECRET_KEY,
