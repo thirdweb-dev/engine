a79e3875596fd9803930a2731272b9c66ee2cbf4
Adam Majmudar <64697628+adam-maj@users.noreply.github.com>
2023-09-22 00:31:41 -0700

    Add support for account & accountFactory extensions
    * Move env to src
    
    * Initial smart wallet creation endpoint
    
    * Update smart wallets
    
    * Add smart wallets to getWallet
    
    * Remove environment variable
    
    * Add support for accountFactory
    
    * Add accountFactory routes to router
    
    * Add routes for account extension
    
    * Change order
    
    * Update to use transaction prepare
    
    * Add x-wallet-address to account write calls
    
    * Update schema.prisma

---
+++ server/api/contract/extensions/account/index.ts
@@ -0,0 +1,21 @@
+import { FastifyInstance } from "fastify";
+import { getAllAdmins } from "./read/getAllAdmins";
+import { getAllSessions } from "./read/getAllSessions";
+import { grantAdmin } from "./write/grantAdmin";
+import { grantSession } from "./write/grantSession";
+import { revokeAdmin } from "./write/revokeAdmin";
+import { revokeSession } from "./write/revokeSession";
+import { updateSession } from "./write/updateSession";
+
+export const accountRoutes = async (fastify: FastifyInstance) => {
+  // GET
+  await fastify.register(getAllAdmins);
+  await fastify.register(getAllSessions);
+
+  // POST
+  await fastify.register(grantAdmin);
+  await fastify.register(revokeAdmin);
+  await fastify.register(grantSession);
+  await fastify.register(revokeSession);
+  await fastify.register(updateSession);
+};
---
+++ server/api/contract/extensions/account/write/revokeAdmin.ts
@@ -0,0 +1,62 @@
+import { Static, Type } from "@sinclair/typebox";
+import { FastifyInstance } from "fastify";
+import { StatusCodes } from "http-status-codes";
+import { walletAuthSchema } from "../../../../../../core/schema";
+import { queueTx } from "../../../../../../src/db/transactions/queueTx";
+import {
+  contractParamSchema,
+  standardResponseSchema,
+  transactionWritesResponseSchema,
+} from "../../../../../helpers";
+import { getChainIdFromChain } from "../../../../../utilities/chain";
+import { getContract } from "../../../../../utils/cache/getContract";
+
+const BodySchema = Type.Object({
+  wallet_address: Type.String({
+    description: "Address to revoke admin permissions from",
+  }),
+});
+
+export const revokeAdmin = async (fastify: FastifyInstance) => {
+  fastify.route<{
+    Params: Static<typeof contractParamSchema>;
+    Reply: Static<typeof transactionWritesResponseSchema>;
+    Body: Static<typeof BodySchema>;
+  }>({
+    method: "POST",
+    url: "/contract/:chain/:contract_address/account/admins/revoke",
+    schema: {
+      description: "Revoke a wallet's admin permissions",
+      tags: ["Account"],
+      operationId: "account:revoke-admin",
+      headers: walletAuthSchema,
+      params: contractParamSchema,
+      body: BodySchema,
+      response: {
+        ...standardResponseSchema,
+        [StatusCodes.OK]: transactionWritesResponseSchema,
+      },
+    },
+    handler: async (req, rep) => {
+      const { chain, contract_address } = req.params;
+      const { wallet_address } = req.body;
+      const walletAddress = req.headers["x-wallet-address"] as string;
+      const chainId = getChainIdFromChain(chain);
+
+      const contract = await getContract({
+        chainId,
+        contractAddress: contract_address,
+        walletAddress,
+      });
+
+      const tx = await contract.account.revokeAdminPermissions.prepare(
+        wallet_address,
+      );
+      const queueId = await queueTx({ tx, chainId, extension: "account" });
+
+      rep.status(StatusCodes.OK).send({
+        result: queueId,
+      });
+    },
+  });
+};
