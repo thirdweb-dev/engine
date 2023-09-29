b009cf4059706d966a70b52a9ba2e57c5c4f3318
farhanW3 <farhan@thirdweb.com>
2023-09-29 12:59:27 -0700

    copy update

--- server/api/transaction/cancel.ts
+++ server/api/transaction/cancel.ts
@@ -29,7 +29,7 @@ export const responseBodySchema = Type.Object({
     }),
     transactionHash: Type.Optional(
       Type.String({
-        description: "Transaction Queue ID",
+        description: "Transaction Hash of the on-chain cancel transaction",
         examples: [
           "0x0514076b5b7e3062c8dc17e10f7c0befe88e6efb7e97f16e3c14afb36c296467",
         ],
