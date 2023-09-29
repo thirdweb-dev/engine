58770bd69a9e643926df21561cb96aba1d8945d5
farhanW3 <farhan@thirdweb.com>
2023-09-28 18:40:55 -0700

    removed extra alter statements

--- src/prisma/migrations/20230927004337_transactions_cancelled_at_added/migration.sql
+++ src/prisma/migrations/20230927004337_transactions_cancelled_at_added/migration.sql
@@ -1,5 +1,2 @@
 -- AlterTable
-ALTER TABLE "transactions" ADD COLUMN     "cancelledAt" TIMESTAMP(3),
-ALTER COLUMN "callGasLimit" SET DATA TYPE TEXT,
-ALTER COLUMN "preVerificationGas" SET DATA TYPE TEXT,
-ALTER COLUMN "verificationGasLimit" SET DATA TYPE TEXT;
+ALTER TABLE "transactions" ADD COLUMN     "cancelledAt" TIMESTAMP(3);
