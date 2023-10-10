93bc5eb88eb3ba72f9cdc10af55b806e6fd46f0a
Adam Majmudar <mr.adam.maj@gmail.com>
2023-10-10 15:56:53 -0700

    Add drop table migration

---
+++ src/prisma/migrations/20231010225604_remove_cancelled_txs/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the `cancelled_transactions` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "cancelled_transactions";
