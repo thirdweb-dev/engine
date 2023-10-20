af2a58e687fbc7e8d1a8d1f7ef5d90c346adfd5f
farhanW3 <farhan@thirdweb.com>
2023-10-20 16:55:09 -0700

    transaction query restricted to last 24 hrs

--- src/db/transactions/getAllTxs.ts
+++ src/db/transactions/getAllTxs.ts
@@ -1,3 +1,4 @@
+import { Transactions } from "@prisma/client";
 import { Static } from "@sinclair/typebox";
 import {
   TransactionStatusEnum,
@@ -16,52 +17,77 @@ interface GetAllTxsParams {
   extensions?: ContractExtension[];
 }
 
+interface GetAllTxsResponse {
+  transactions: Static<typeof transactionResponseSchema>[];
+  totalCount: number;
+}
+
 export const getAllTxs = async ({
   pgtx,
   page,
   limit,
   filter,
   extensions,
-}: GetAllTxsParams): Promise<Static<typeof transactionResponseSchema>[]> => {
-  let filterBy:
-    | "queuedAt"
-    | "sentAt"
-    | "processedAt"
-    | "minedAt"
-    | "errorMessage"
-    | undefined;
-
-  if (filter === TransactionStatusEnum.Queued) {
-    filterBy = "queuedAt";
-  } else if (filter === TransactionStatusEnum.Submitted) {
-    filterBy = "sentAt";
-  } else if (filter === TransactionStatusEnum.Processed) {
-    filterBy = "processedAt";
-  } else if (filter === TransactionStatusEnum.Mined) {
-    filterBy = "minedAt";
-  } else if (filter === TransactionStatusEnum.Errored) {
-    filterBy = "errorMessage";
-  }
-
+}: GetAllTxsParams): Promise<GetAllTxsResponse> => {
   const prisma = getPrismaWithPostgresTx(pgtx);
 
+  // TODO: To bring this back for Paid feature
+  // let filterBy:
+  //   | "queuedAt"
+  //   | "sentAt"
+  //   | "processedAt"
+  //   | "minedAt"
+  //   | "errorMessage"
+  //   | undefined;
+
+  // if (filter === TransactionStatusEnum.Queued) {
+  //   filterBy = "queuedAt";
+  // } else if (filter === TransactionStatusEnum.Submitted) {
+  //   filterBy = "sentAt";
+  // } else if (filter === TransactionStatusEnum.Processed) {
+  //   filterBy = "processedAt";
+  // } else if (filter === TransactionStatusEnum.Mined) {
+  //   filterBy = "minedAt";
+  // } else if (filter === TransactionStatusEnum.Errored) {
+  //   filterBy = "errorMessage";
+  // }
+
+  // const filterQuery = {
+  //   ...(filterBy
+  //     ? {
+  //         [filterBy]: {
+  //           not: null,
+  //         },
+  //       }
+  //     : {}),
+  //   ...(extensions
+  //     ? {
+  //         extension: {
+  //           in: extensions,
+  //         },
+  //       }
+  //     : {}),
+  // };
+
   // TODO: Cleaning should be handled by zod
-  const txs = await prisma.transactions.findMany({
+  const totalCountPromise: Promise<number> = prisma.transactions.count({
     where: {
-      ...(filterBy
-        ? {
-            [filterBy]: {
-              not: null,
-            },
-          }
-        : {}),
-      ...(extensions
-        ? {
-            extension: {
-              in: extensions,
-            },
-          }
-        : {}),
+      // TODO: To bring this back for Paid feature
+      // ...filterQuery,
+      queuedAt: {
+        gte: new Date(Date.now() - 24 * 60 * 60 * 1000),
+      },
+    },
+  });
+
+  // TODO: Cleaning should be handled by zod
+  const txsPromise: Promise<Transactions[]> = prisma.transactions.findMany({
+    where: {
+      // TODO: To bring this back for Paid feature
+      // ...filterQuery,
+      queuedAt: {
+        gte: new Date(Date.now() - 24 * 60 * 60 * 1000),
+      },
     },
     orderBy: [
       {
@@ -72,5 +98,10 @@ export const getAllTxs = async ({
     take: limit,
   });
 
-  return cleanTxs(txs);
+  const [totalCount, txs] = await Promise.all([totalCountPromise, txsPromise]);
+
+  return {
+    transactions: cleanTxs(txs),
+    totalCount: totalCount,
+  };
 };
