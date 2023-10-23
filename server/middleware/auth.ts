826e88809dab0dd6f65e2d639f85e3272f6d6d09
Adam Majmudar <mr.adam.maj@gmail.com>
2023-10-23 16:46:39 -0700

    Allow thirdweb-preview.com for api-server auht

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,4 +1,7 @@
+import { Json, User, authenticateJWT } from "@thirdweb-dev/auth";
 import { ThirdwebAuthUser } from "@thirdweb-dev/auth/fastify";
+import { GenericAuthWallet } from "@thirdweb-dev/wallets";
+import { utils } from "ethers";
 
 export type TAuthData = never;
 export type TAuthSession = { permissions: string };
@@ -8,3 +11,45 @@ declare module "fastify" {
     user: ThirdwebAuthUser<TAuthData, TAuthSession>;
   }
 }
+
+export const authWithApiServer = async (jwt: string, domain: string) => {
+  let user: User<Json> | null = null;
+  try {
+    user = await authenticateJWT({
+      wallet: {
+        type: "evm",
+        getAddress: async () => "0x016757dDf2Ab6a998a4729A80a091308d9059E17",
+        verifySignature: async (
+          message: string,
+          signature: string,
+          address: string,
+        ) => {
+          try {
+            const messageHash = utils.hashMessage(message);
+            const messageHashBytes = utils.arrayify(messageHash);
+            const recoveredAddress = utils.recoverAddress(
+              messageHashBytes,
+              signature,
+            );
+
+            if (recoveredAddress === address) {
+              return true;
+            }
+          } catch {
+            // no-op
+          }
+
+          return false;
+        },
+      } as GenericAuthWallet,
+      jwt,
+      options: {
+        domain,
+      },
+    });
+  } catch {
+    // no-op
+  }
+
+  return user;
+};
