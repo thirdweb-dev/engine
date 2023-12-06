51a24c06afc1119449c60034aa24f90cc8f32e88
Adam Majmudar <mr.adam.maj@gmail.com>
2023-12-06 15:54:53 -0800

    Add pagination to getAllWallets

--- src/db/wallets/getAllWallets.ts
+++ src/db/wallets/getAllWallets.ts
@@ -3,12 +3,21 @@ import { getPrismaWithPostgresTx } from "../client";
 
 interface GetAllWalletsParams {
   pgtx?: PrismaTransaction;
+  page: number;
+  limit: number;
 }
 
 // TODO: Add error logging handler from req.log to all queries
-export const getAllWallets = async ({ pgtx }: GetAllWalletsParams = {}) => {
+export const getAllWallets = async ({
+  pgtx,
+  page,
+  limit,
+}: GetAllWalletsParams) => {
   const prisma = getPrismaWithPostgresTx(pgtx);
 
-  const wallets = await prisma.walletDetails.findMany();
+  const wallets = await prisma.walletDetails.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
+  });
   return wallets;
 };
