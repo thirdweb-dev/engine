8dc368e678a279185fd3077899948e099446190e
farhanW3 <farhan@thirdweb.com>
2024-01-11 22:19:43 -0800

    santinized origins config

--- src/server/middleware/cors/cors.ts
+++ src/server/middleware/cors/cors.ts
@@ -102,6 +102,32 @@ const defaultOptions = {
   strictPreflight: true,
 };
 
+const sanitizeOrigin = (data: string): string | RegExp => {
+  if (data.startsWith("/") && data.endsWith("/")) {
+    return new RegExp(data.slice(1, -1));
+  }
+
+  if (data.startsWith("*.")) {
+    const regex = data.replace("*.", ".*.");
+    return new RegExp(regex);
+  }
+
+  if (data.includes("thirdweb-preview.com")) {
+    return new RegExp(/^https?:\/\/.*\.thirdweb-preview\.com$/);
+  }
+  if (data.includes("thirdweb-dev.com")) {
+    return new RegExp(/^https?:\/\/.*\.thirdweb-dev\.com$/);
+  }
+
+  // Remove trailing slashes.
+  // The origin header does not include a trailing slash.
+  if (data.endsWith("/")) {
+    return data.slice(0, -1);
+  }
+
+  return data;
+};
+
 export const fastifyCors = async (
   fastify: FastifyInstance,
   req: FastifyRequest,
@@ -112,7 +138,7 @@ export const fastifyCors = async (
   const config = await getConfig();
 
   const originArray = config.accessControlAllowOrigin.split(",") as string[];
-  opts.origin = originArray;
+  opts.origin = originArray.map(sanitizeOrigin);
 
   let hideOptionsRoute = true;
   if (opts.hideOptionsRoute !== undefined) {
