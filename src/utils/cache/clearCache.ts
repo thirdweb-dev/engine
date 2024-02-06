1c47eee390a6932783db38a082f83c05dd2dc1ef
Phillip Ho <arcoraven@gmail.com>
2024-02-05 16:46:32 -0800

    fix: Use a single Node entrypoint (#397)
    * fix: Use a single Node entrypoint
    
    * remove debug line

--- src/utils/cache/clearCache.ts
+++ src/utils/cache/clearCache.ts
@@ -1,5 +1,4 @@
 import { env } from "../env";
-import { logger } from "../logger";
 import { configCache } from "./getConfig";
 import { sdkCache } from "./getSdk";
 import { walletsCache } from "./getWallet";
@@ -8,12 +7,6 @@ import { webhookCache } from "./getWebhook";
 export const clearCache = async (
   service: (typeof env)["LOG_SERVICES"][0],
 ): Promise<void> => {
-  logger({
-    level: "info",
-    service,
-    message: "Resetting Cache",
-  });
-
   // Reset config
   configCache.clear();
 
