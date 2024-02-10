6a6e93be06a9c73e14e969220b06f09659cd2b61
farhanW3 <farhan@thirdweb.com>
2024-02-09 22:53:10 -0800

    sdk updates + new SDK configs

--- src/utils/cache/getSdk.ts
+++ src/utils/cache/getSdk.ts
@@ -112,6 +112,10 @@ export const getSdk = async ({
     sdk = new ThirdwebSDK(chain!, {
       secretKey: env.THIRDWEB_API_SECRET_KEY,
       supportedChains: config.chainOverrides ? RPC_OVERRIDES : undefined,
+      rpcBatchSettings: {
+        sizeLimit: 100,
+        timeLimit: env.SDK_BATCH_TIME_LIMIT,
+      },
     });
   } else {
     const wallet = await getWallet({
@@ -123,6 +127,10 @@ export const getSdk = async ({
     sdk = await ThirdwebSDK.fromWallet(wallet, chainId, {
       secretKey: env.THIRDWEB_API_SECRET_KEY,
       supportedChains: config.chainOverrides ? RPC_OVERRIDES : undefined,
+      rpcBatchSettings: {
+        sizeLimit: 100,
+        timeLimit: 0,
+      },
     });
   }
 
