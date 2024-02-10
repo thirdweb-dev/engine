89d3fa2db98b11c741f3e5e0fca5d85371ce9398
farhanW3 <farhan@thirdweb.com>
2024-02-09 22:54:12 -0800

    config upds

--- src/utils/cache/getSdk.ts
+++ src/utils/cache/getSdk.ts
@@ -113,7 +113,7 @@ export const getSdk = async ({
       secretKey: env.THIRDWEB_API_SECRET_KEY,
       supportedChains: config.chainOverrides ? RPC_OVERRIDES : undefined,
       rpcBatchSettings: {
-        sizeLimit: 100,
+        sizeLimit: env.SDK_BATCH_SIZE_LIMIT,
         timeLimit: env.SDK_BATCH_TIME_LIMIT,
       },
     });
@@ -128,8 +128,8 @@ export const getSdk = async ({
       secretKey: env.THIRDWEB_API_SECRET_KEY,
       supportedChains: config.chainOverrides ? RPC_OVERRIDES : undefined,
       rpcBatchSettings: {
-        sizeLimit: 100,
-        timeLimit: 0,
+        sizeLimit: env.SDK_BATCH_SIZE_LIMIT,
+        timeLimit: env.SDK_BATCH_TIME_LIMIT,
       },
     });
   }
