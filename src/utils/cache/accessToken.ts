393d9e3dd5d5779c0ac6a0754d2a22fd764f6625
Phillip Ho <arcoraven@gmail.com>
2024-02-15 20:02:40 -0800

    feat: clean up auth flows

---
+++ src/utils/cache/accessToken.ts
@@ -0,0 +1,22 @@
+import type { Tokens } from "@prisma/client";
+import { getToken } from "../../db/tokens/getToken";
+
+// Cache an access token JWT to the token object, or null if not found.
+export const accessTokenCache = new Map<string, Tokens | null>();
+
+interface GetAccessTokenParams {
+  jwt: string;
+}
+
+export const getAccessToken = async ({
+  jwt,
+}: GetAccessTokenParams): Promise<Tokens | null> => {
+  const cached = accessTokenCache.get(jwt);
+  if (cached) {
+    return cached;
+  }
+
+  const accessToken = await getToken({ jwt });
+  accessTokenCache.set(jwt, accessToken);
+  return accessToken;
+};
