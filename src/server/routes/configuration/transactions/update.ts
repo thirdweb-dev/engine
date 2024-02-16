c3539485b7e8614ed47c158b40846684ce4e87d3
farhanW3 <farhan@thirdweb.com>
2024-02-15 17:58:14 -0800

    config/transactions updates

--- src/server/routes/configuration/transactions/update.ts
+++ src/server/routes/configuration/transactions/update.ts
@@ -4,7 +4,6 @@ import { StatusCodes } from "http-status-codes";
 import { updateConfiguration } from "../../../../db/configuration/updateConfiguration";
 import { getConfig } from "../../../../utils/cache/getConfig";
 import { standardResponseSchema } from "../../../schemas/sharedApiSchemas";
-import { ReplySchema } from "./get";
 
 const BodySchema = Type.Partial(
   Type.Object({
@@ -19,6 +18,20 @@ const BodySchema = Type.Partial(
     maxRetriesPerTx: Type.Number(),
   }),
 );
+
+const ReplySchema = Type.Object({
+  result: Type.Object({
+    minTxsToProcess: Type.Number(),
+    maxTxsToProcess: Type.Number(),
+    minedTxListenerCronSchedule: Type.Union([Type.String(), Type.Null()]),
+    maxTxsToUpdate: Type.Number(),
+    retryTxListenerCronSchedule: Type.Union([Type.String(), Type.Null()]),
+    minEllapsedBlocksBeforeRetry: Type.Number(),
+    maxFeePerGasForRetries: Type.String(),
+    maxPriorityFeePerGasForRetries: Type.String(),
+    maxRetriesPerTx: Type.Number(),
+  }),
+});
 
 export async function updateTransactionConfiguration(fastify: FastifyInstance) {
   fastify.route<{
