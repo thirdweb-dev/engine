50fe3277cd6ea1b2fea2aa169aa4934f8897dd86
farhanW3 <132962163+farhanW3@users.noreply.github.com>
2024-02-21 09:02:34 +0530

    removed multi-cron run check (#418)

--- src/worker/listeners/minedTxListener.ts
+++ src/worker/listeners/minedTxListener.ts
@@ -1,11 +1,9 @@
 import cron from "node-cron";
 import { getConfig } from "../../utils/cache/getConfig";
-import { logger } from "../../utils/logger";
 import { updateMinedTx } from "../tasks/updateMinedTx";
 import { updateMinedUserOps } from "../tasks/updateMinedUserOps";
 
 let task: cron.ScheduledTask;
-let minedTxStarted = false;
 export const minedTxListener = async () => {
   const config = await getConfig();
 
@@ -18,17 +16,7 @@ export const minedTxListener = async () => {
   }
 
   task = cron.schedule(config.minedTxListenerCronSchedule, async () => {
-    if (!minedTxStarted) {
-      minedTxStarted = true;
-      await updateMinedTx();
-      await updateMinedUserOps();
-      minedTxStarted = false;
-    } else {
-      logger({
-        service: "worker",
-        level: "warn",
-        message: "Mined tx listener already running, skipping",
-      });
-    }
+    await updateMinedTx();
+    await updateMinedUserOps();
   });
 };
