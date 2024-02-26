ad2606cd5d36944e35e2167993bb21dba72e8d8b
Isaac Dubuque <idubuque@umich.edu>
2024-02-26 10:54:15 -0800

    cleaning up migration history

--- src/prisma/migrations/20240226073240_remove_fk_constraints/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "ContractLogs" DROP CONSTRAINT "ContractLogs_chainId_contractAddress_fkey";
-
--- DropForeignKey
-ALTER TABLE "IndexedContracts" DROP CONSTRAINT "IndexedContracts_chainId_fkey";
