666f1e1eb91b813b8ec4f5957923ec004a9b73e1
Farhan Khwaja <132962163+farhanW3@users.noreply.github.com>
2024-05-14 08:58:44 +0530

    Logging Updates (#523)
    * Logging Updates
    
    * removed unwanted onEvents logs

--- src/worker/queues/queues.ts
+++ src/worker/queues/queues.ts
@@ -14,20 +14,6 @@ export const defaultJobOptions: JobsOptions = {
 };
 
 export const logWorkerEvents = (worker: Worker) => {
-  worker.on("active", (job: Job) => {
-    logger({
-      level: "debug",
-      message: `[${worker.name}] Processing: ${job.id}`,
-      service: "worker",
-    });
-  });
-  worker.on("completed", (job: Job) => {
-    logger({
-      level: "debug",
-      message: `[${worker.name}] Completed: ${job.id}`,
-      service: "worker",
-    });
-  });
   worker.on("failed", (job: Job | undefined, err: Error) => {
     logger({
       level: "error",
