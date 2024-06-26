7a51779c58b441d9dab13bb0811e7cd420aae973
Phillip Ho <arcoraven@gmail.com>
2024-06-25 17:46:20 -0700

    feat: Support v5 format for signatureGenerate endpoints

--- src/server/routes/contract/extensions/erc20/read/signatureGenerate.ts
+++ src/server/routes/contract/extensions/erc20/read/signatureGenerate.ts
@@ -1,10 +1,12 @@
 import { Static, Type } from "@sinclair/typebox";
 import { FastifyInstance } from "fastify";
 import { StatusCodes } from "http-status-codes";
-import { defineChain, getContract } from "thirdweb";
+import { Address, Hex, defineChain, getContract } from "thirdweb";
 import { generateMintSignature } from "thirdweb/extensions/erc20";
+import { getAccount } from "../../../../../../utils/account";
 import { getContract as getContractV4 } from "../../../../../../utils/cache/getContract";
 import { thirdwebClient } from "../../../../../../utils/sdk";
+import { createCustomError } from "../../../../../middleware/error";
 import {
   erc20ResponseType,
   signature20InputSchema,
@@ -19,16 +21,51 @@ import { walletWithAAHeaderSchema } from "../../../../../schemas/wallet";
 import { getChainIdFromChain } from "../../../../../utils/chain";
 import { checkAndReturnERC20SignaturePayload } from "../../../../../utils/validator";
 
-// INPUTS
-const requestSchema = erc721ContractParamSchema;
-const requestBodySchema = signature20InputSchema;
+// v4 sdk
+const requestBodySchemaV4 = signature20InputSchema;
+const responseSchemaV4 = Type.Object({
+  payload: signature20OutputSchema,
+  signature: Type.String(),
+});
 
-// OUTPUT
-const responseSchema = Type.Object({
-  result: Type.Object({
-    payload: signature20OutputSchema,
-    signature: Type.String(),
+// v5 sdk
+const requestBodySchemaV5 = Type.Intersect([
+  Type.Object({
+    to: Type.String(),
+    primarySaleRecipient: Type.Optional(Type.String()),
+    price: Type.Optional(Type.String()),
+    priceInWei: Type.Optional(Type.String()),
+    currency: Type.Optional(Type.String()),
+    validityStartTimestamp: Type.Integer(),
+    validityEndTimestamp: Type.Optional(Type.Integer()),
+    uid: Type.Optional(Type.String()),
   }),
+  Type.Union([
+    Type.Object({ quantity: Type.String() }),
+    Type.Object({ quantityWei: Type.String() }),
+  ]),
+]);
+const responseSchemaV5 = Type.Object({
+  payload: Type.Object({
+    to: Type.String(),
+    primarySaleRecipient: Type.String(),
+    quantity: Type.String(),
+    price: Type.String(),
+    currency: Type.String(),
+    validityStartTimestamp: Type.Integer(),
+    validityEndTimestamp: Type.Integer(),
+    uid: Type.String(),
+  }),
+  signature: Type.String(),
+});
+
+const requestSchema = erc721ContractParamSchema;
+const requestBodySchema = Type.Union([
+  requestBodySchemaV4,
+  requestBodySchemaV5,
+]);
+const responseSchema = Type.Object({
+  result: Type.Union([responseSchemaV4, responseSchemaV5]),
 });
 
 responseSchema.example = {
@@ -64,6 +101,86 @@ export async function erc20SignatureGenerate(fastify: FastifyInstance) {
     handler: async (request, reply) => {
       const { chain, contractAddress } = request.params;
       const {
+        "x-backend-wallet-address": walletAddress,
+        "x-account-address": accountAddress,
+      } = request.headers as Static<typeof walletWithAAHeaderSchema>;
+      const { "x-thirdweb-sdk-version": sdkVersion } =
+        request.headers as Static<typeof thirdwebSdkVersionSchema>;
+
+      const chainId = await getChainIdFromChain(chain);
+
+      // Use v5 SDK if "x-thirdweb-sdk-version" header is set.
+      if (sdkVersion === "5") {
+        const args = request.body as Static<typeof requestBodySchemaV5>;
+        const {
+          to,
+          primarySaleRecipient,
+          price,
+          priceInWei,
+          currency,
+          validityStartTimestamp,
+          validityEndTimestamp,
+          uid,
+        } = args;
+
+        let quantityInput: { quantity: string } | { quantityWei: bigint };
+        if ("quantity" in args) {
+          quantityInput = { quantity: args.quantity };
+        } else if ("quantityWei" in args) {
+          quantityInput = { quantityWei: BigInt(args.quantityWei) };
+        } else {
+          throw createCustomError(
+            `Missing "quantity" or "quantityWei".`,
+            StatusCodes.BAD_REQUEST,
+            "MISSING_PARAMETERS",
+          );
+        }
+
+        const contract = getContract({
+          client: thirdwebClient,
+          chain: defineChain(chainId),
+          address: contractAddress,
+        });
+        const account = await getAccount({
+          chainId,
+          from: walletAddress as Address,
+          accountAddress: accountAddress as Address,
+        });
+
+        const { payload, signature } = await generateMintSignature({
+          contract,
+          account,
+          mintRequest: {
+            to,
+            primarySaleRecipient,
+            price,
+            priceInWei: priceInWei ? BigInt(priceInWei) : undefined,
+            currency: currency as Address | undefined,
+            validityStartTimestamp: new Date(validityStartTimestamp * 1000),
+            validityEndTimestamp: validityEndTimestamp
+              ? new Date(validityEndTimestamp * 1000)
+              : undefined,
+            uid: uid as Hex | undefined,
+            ...quantityInput,
+          },
+        });
+
+        return reply.status(StatusCodes.OK).send({
+          result: {
+            payload: {
+              ...payload,
+              quantity: payload.quantity.toString(),
+              price: payload.price.toString(),
+              validityStartTimestamp: Number(payload.validityStartTimestamp),
+              validityEndTimestamp: Number(payload.validityEndTimestamp),
+            },
+            signature,
+          },
+        });
+      }
+
+      // Use v4 SDK.
+      const {
         to,
         currencyAddress,
         mintEndTime,
@@ -72,15 +189,7 @@ export async function erc20SignatureGenerate(fastify: FastifyInstance) {
         primarySaleRecipient,
         quantity,
         uid,
-      } = request.body;
-      const {
-        "x-backend-wallet-address": walletAddress,
-        "x-account-address": accountAddress,
-        "x-thirdweb-sdk-version": sdkVersion,
-      } = request.headers as Static<typeof walletWithAAHeaderSchema> &
-        Static<typeof thirdwebSdkVersionSchema>;
-
-      const chainId = await getChainIdFromChain(chain);
+      } = request.body as Static<typeof requestBodySchemaV4>;
 
       const contract = await getContractV4({
         chainId,
@@ -88,23 +197,6 @@ export async function erc20SignatureGenerate(fastify: FastifyInstance) {
         walletAddress,
         accountAddress,
       });
-
-      if (sdkVersion === "5") {
-        const contract = getContract({
-          client: thirdwebClient,
-          chain: defineChain(chainId),
-          address: contractAddress,
-        });
-        const { payload, signature } = await generateMintSignature({
-          account,
-          contract,
-          mintRequest: {
-            to: "0x...",
-            quantity: "10",
-          },
-        });
-        return;
-      }
 
       const payload = checkAndReturnERC20SignaturePayload<
         Static<typeof signature20InputSchema>,
