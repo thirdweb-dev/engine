59d836f6f4def7a561a90901cf53f65448ad4949
farhanW3 <farhan@thirdweb.com>
2024-07-18 19:37:02 -0700

    updated error handling

--- src/server/middleware/error.ts
+++ src/server/middleware/error.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
+import { ZodError } from "zod";
 import { env } from "../../utils/env";
 import { logger } from "../../utils/logger";
 
@@ -20,6 +21,46 @@ export const createCustomError = (
   code,
 });
 
+const ETHERS_ERROR_CODES = [
+  "UNKNOWN_ERROR",
+  "NOT_IMPLEMENTED",
+  "UNSUPPORTED_OPERATION",
+  "NETWORK_ERROR",
+  "SERVER_ERROR",
+  "TIMEOUT",
+  "BAD_DATA",
+  "CANCELLED",
+  "BUFFER_OVERRUN",
+  "NUMERIC_FAULT",
+  "INVALID_ARGUMENT",
+  "MISSING_ARGUMENT",
+  "UNEXPECTED_ARGUMENT",
+  "VALUE_MISMATCH",
+  "CALL_EXCEPTION",
+  "INSUFFICIENT_FUNDS",
+  "NONCE_EXPIRED",
+  "REPLACEMENT_UNDERPRICED",
+  "TRANSACTION_REPLACED",
+  "UNCONFIGURED_NAME",
+  "OFFCHAIN_FAULT",
+  "ACTION_REJECTED",
+];
+
+const isZodError = (err: unknown): boolean => {
+  return Boolean(
+    err && (err instanceof ZodError || (err as ZodError).name === "ZodError"),
+  );
+};
+
+const isEthersError = (error: any): boolean => {
+  return (
+    error &&
+    typeof error === "object" &&
+    "code" in error &&
+    ETHERS_ERROR_CODES.includes(error.code)
+  );
+};
+
 export const createCustomDateTimestampError = (key: string): CustomError => {
   return createCustomError(
     `Invalid ${key} Value. Needs to new Date() / new Date().toISOstring() / new Date().getTime() / Unix Epoch`,
@@ -32,68 +73,84 @@ const flipObject = (data: any) =>
   Object.fromEntries(Object.entries(data).map(([key, value]) => [value, key]));
 
 export const withErrorHandler = async (server: FastifyInstance) => {
-  server.setErrorHandler((error: Error | CustomError, request, reply) => {
-    logger({
-      service: "server",
-      level: "error",
-      message: `Encountered server error`,
-      error,
-    });
+  server.setErrorHandler(
+    (error: Error | CustomError | ZodError, request, reply) => {
+      logger({
+        service: "server",
+        level: "error",
+        message: `Encountered server error`,
+        error,
+      });
 
-    // v4 : Contract Prepare Error Handler
-    if (error.message.includes("code=INVALID_ARGUMENT")) {
-      return reply.status(StatusCodes.BAD_REQUEST).send({
+      // Ethers Error Codes
+      if (isEthersError(error)) {
+        return reply.status(StatusCodes.BAD_REQUEST).send({
+          error: {
+            code: "BAD_REQUEST",
+            message: (error as any).code,
+            reason: error.message,
+            statusCode: 400,
+            stack: env.NODE_ENV !== "production" ? error.stack : undefined,
+          },
+        });
+      }
+
+      // Zod Typings Errors
+      if (isZodError(error)) {
+        const _error = error as ZodError;
+        let parsedMessage: any[] = [];
+
+        try {
+          parsedMessage = JSON.parse(_error.message);
+        } catch (e) {
+          console.error("Failed to parse error message:", e);
+        }
+        const errorObject =
+          Array.isArray(parsedMessage) && parsedMessage.length > 0
+            ? parsedMessage[0]
+            : {};
+
+        return reply.status(StatusCodes.BAD_REQUEST).send({
+          error: {
+            code: "BAD_REQUEST",
+            message: errorObject.message ?? "Invalid Request",
+            reason: errorObject ?? undefined,
+            statusCode: 400,
+            stack: env.NODE_ENV !== "production" ? _error.stack : undefined,
+          },
+        });
+      }
+
+      // Handle Custom Errors
+      if ("statusCode" in error && "code" in error) {
+        // Transform unexpected errors into a standard payload
+        const statusCode =
+          error.statusCode ?? StatusCodes.INTERNAL_SERVER_ERROR;
+        const code =
+          error.code ??
+          flipObject(StatusCodes)[statusCode] ??
+          StatusCodes.INTERNAL_SERVER_ERROR;
+
+        const message = error.message ?? ReasonPhrases.INTERNAL_SERVER_ERROR;
+        return reply.status(statusCode).send({
+          error: {
+            code,
+            message,
+            statusCode,
+            stack: env.NODE_ENV !== "production" ? error.stack : undefined,
+          },
+        });
+      }
+
+      // Handle non-custom errors
+      return reply.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
         error: {
-          code: "BAD_REQUEST",
-          message: "INVALID_ARGUMENT",
-          reason: error.message,
-          statusCode: 400,
+          statusCode: 500,
+          code: "INTERNAL_SERVER_ERROR",
+          message: error.message || ReasonPhrases.INTERNAL_SERVER_ERROR,
           stack: env.NODE_ENV !== "production" ? error.stack : undefined,
         },
       });
-    }
-
-    // v4: Zod Typings Errors
-    if (error.message.toLowerCase().includes("invalid input")) {
-      return reply.status(StatusCodes.BAD_REQUEST).send({
-        error: {
-          code: "BAD_REQUEST",
-          message: "Invalid input",
-          reason: JSON.parse(error.message),
-          statusCode: 400,
-          stack: env.NODE_ENV !== "production" ? error.stack : undefined,
-        },
-      });
-    }
-
-    // Handle Custom Errors
-    if ("statusCode" in error && "code" in error) {
-      // Transform unexpected errors into a standard payload
-      const statusCode = error.statusCode ?? StatusCodes.INTERNAL_SERVER_ERROR;
-      const code =
-        error.code ??
-        flipObject(StatusCodes)[statusCode] ??
-        StatusCodes.INTERNAL_SERVER_ERROR;
-
-      const message = error.message ?? ReasonPhrases.INTERNAL_SERVER_ERROR;
-      return reply.status(statusCode).send({
-        error: {
-          code,
-          message,
-          statusCode,
-          stack: env.NODE_ENV !== "production" ? error.stack : undefined,
-        },
-      });
-    }
-
-    // Handle non-custom errors
-    return reply.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
-      error: {
-        statusCode: 500,
-        code: "INTERNAL_SERVER_ERROR",
-        message: error.message || ReasonPhrases.INTERNAL_SERVER_ERROR,
-        stack: env.NODE_ENV !== "production" ? error.stack : undefined,
-      },
-    });
-  });
+    },
+  );
 };
