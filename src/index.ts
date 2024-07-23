b4723ce6e8b4889aea71dca80483a2c5052f7a0b
farhanW3 <farhan@thirdweb.com>
2024-07-23 12:19:57 -0700

    Updates: Added global error handlers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import "./polyfill";
 import { initServer } from "./server";
+import { logger } from "./utils/logger";
 import { initWorker } from "./worker";
 
 const main = async () => {
@@ -8,3 +9,26 @@ const main = async () => {
 };
 
 main();
+
+// Adding handlers for `uncaughtException` & `unhandledRejection`
+// Needs to be root level of your application to ensure it
+// catches any unhandled exceptions that occur throughout
+// entire codebase
+
+process.on("uncaughtException", (err) => {
+  logger({
+    message: "Uncaught Exception",
+    service: "server",
+    level: "error",
+    error: err,
+  });
+});
+
+process.on("unhandledRejection", (err) => {
+  logger({
+    message: "Unhandled Rejection",
+    service: "server",
+    level: "error",
+    error: err,
+  });
+});
