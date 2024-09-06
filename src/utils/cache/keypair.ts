943fd7d91df32a392b803423a8cf6c81478ca629
Phillip Ho <arcoraven@gmail.com>
2024-09-06 10:09:10 +0900

    chore: accept public key ID in kid header

--- src/utils/cache/keypair.ts
+++ src/utils/cache/keypair.ts
@@ -1,20 +1,36 @@
 import type { Keypairs } from "@prisma/client";
-import { getKeypairByPublicKey } from "../../db/keypair/get";
+import { getKeypairByHash, getKeypairByPublicKey } from "../../db/keypair/get";
 
 // Cache a public key to the Keypair object, or null if not found.
 export const keypairCache = new Map<string, Keypairs | null>();
 
-export const getKeypair = async ({
-  publicKey,
-}: {
-  publicKey: string;
-}): Promise<Keypairs | null> => {
-  const cached = keypairCache.get(publicKey);
+/**
+ * Get a keypair by public key or hash.
+ */
+export const getKeypair = async (
+  args:
+    | {
+        publicKey: string;
+      }
+    | {
+        publicKeyHash: string;
+      },
+): Promise<Keypairs | null> => {
+  const hasPublicKey = "publicKey" in args;
+
+  const key = hasPublicKey
+    ? `public-key:${args.publicKey}`
+    : `public-key-hash:${args.publicKeyHash}`;
+
+  const cached = keypairCache.get(key);
   if (cached) {
     return cached;
   }
 
-  const keypair = await getKeypairByPublicKey({ publicKey });
-  keypairCache.set(publicKey, keypair);
+  const keypair = hasPublicKey
+    ? await getKeypairByPublicKey(args.publicKey)
+    : await getKeypairByHash(args.publicKeyHash);
+
+  keypairCache.set(key, keypair);
   return keypair;
 };
