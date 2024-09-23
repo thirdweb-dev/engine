88e5efbd4258005f13cfecda75d80ab151e60d57
Phillip Ho <arcoraven@gmail.com>
2024-09-24 07:46:30 +0800

    Revert "chore: port /contract/write to V5 (#661)"
    This reverts commit 044f922b6f5d52516e07c66ec35354053bc12a1a.

--- src/utils/cache/getContractv5.ts
+++ src/utils/cache/getContractv5.ts
@@ -4,14 +4,12 @@ import { thirdwebClient } from "../../utils/sdk";
 interface GetContractParams {
   chainId: number;
   contractAddress: string;
-  abi?: any;
 }
 
 // Using new v5 SDK
 export const getContractV5 = ({
   chainId,
   contractAddress,
-  abi,
 }: GetContractParams) => {
   const definedChain = defineChain(chainId);
 
@@ -23,6 +21,5 @@ export const getContractV5 = ({
     address: contractAddress,
     // the chain the contract is deployed on
     chain: definedChain,
-    abi,
   });
 };
