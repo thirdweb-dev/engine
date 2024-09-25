ff6d915d57004a62cb7005a03c26e88f6f95f8e1
Phillip Ho <arcoraven@gmail.com>
2024-09-25 15:02:08 +0800

    chore: consolidate chain queries, 4xx if not found

--- src/server/utils/chain.ts
+++ src/server/utils/chain.ts
@@ -1,64 +1,23 @@
-import { Static } from "@sinclair/typebox";
 import { getChainBySlugAsync } from "@thirdweb-dev/chains";
-import { StatusCodes } from "http-status-codes";
-import { getConfig } from "../../utils/cache/getConfig";
-import { networkResponseSchema } from "../../utils/cache/getSdk";
-import { logger } from "../../utils/logger";
-import { createCustomError } from "../middleware/error";
+import { getChain } from "../../utils/chain";
+import { badChainError } from "../middleware/error";
 
 /**
  * Given a valid chain name ('Polygon') or ID ('137'), return the numeric chain ID.
  * @throws if the chain is invalid or deprecated.
  */
 export const getChainIdFromChain = async (input: string): Promise<number> => {
-  const inputId = parseInt(input);
-  if (!isNaN(inputId)) {
-    // input is a valid number.
-    return inputId;
-  }
+	const chainId = Number.parseInt(input);
+	if (!Number.isNaN(chainId)) {
+		return (await getChain(chainId)).id;
+	}
 
-  // input is non-numeric. Try to resolve the name to the chain ID.
-  const slug = input.toLowerCase();
-  const config = await getConfig();
+	try {
+		const chainV4 = await getChainBySlugAsync(input.toLowerCase());
+		if (chainV4.status !== "deprecated") {
+			return chainV4.chainId;
+		}
+	} catch {}
 
-  // Check if the chain ID exists in chainOverrides first.
-  if (config.chainOverrides) {
-    try {
-      const parsedChainOverrides: Static<typeof networkResponseSchema>[] =
-        JSON.parse(config.chainOverrides);
-
-      for (const chainData of parsedChainOverrides) {
-        if (slug === chainData.slug.toLowerCase()) {
-          return chainData.chainId;
-        }
-      }
-    } catch (e) {
-      logger({
-        service: "server",
-        level: "error",
-        message: `Failed parsing chainOverrides: ${e}`,
-      });
-    }
-  }
-
-  // Fetch by slug.
-  // Throw if the chain is invalid or deprecated.
-  try {
-    const chain = await getChainBySlugAsync(slug);
-    if (chain.status === "deprecated") {
-      throw createCustomError(
-        `Chain ${input} is deprecated`,
-        StatusCodes.BAD_REQUEST,
-        "INVALID_CHAIN",
-      );
-    }
-
-    return chain.chainId;
-  } catch (e) {
-    throw createCustomError(
-      `Chain ${input} is not found`,
-      StatusCodes.BAD_REQUEST,
-      "INVALID_CHAIN",
-    );
-  }
+	throw badChainError(chainId);
 };
--- src/server/utils/wallets/getLocalWallet.ts
+++ src/server/utils/wallets/getLocalWallet.ts
@@ -1,111 +1,91 @@
-import { Static } from "@sinclair/typebox";
-import { getChainByChainIdAsync } from "@thirdweb-dev/chains";
-import { Chain, LocalWallet } from "@thirdweb-dev/wallets";
+import { LocalWallet } from "@thirdweb-dev/wallets";
 import { Wallet } from "ethers";
-import { Address } from "thirdweb";
-import { Account, privateKeyToAccount } from "thirdweb/wallets";
+import type { Address } from "thirdweb";
+import { getChainMetadata } from "thirdweb/chains";
+import { privateKeyToAccount, type Account } from "thirdweb/wallets";
 import { getWalletDetails } from "../../../db/wallets/getWalletDetails";
-import { getConfig } from "../../../utils/cache/getConfig";
-import { networkResponseSchema } from "../../../utils/cache/getSdk";
+import { getChain } from "../../../utils/chain";
 import { env } from "../../../utils/env";
 import { logger } from "../../../utils/logger";
 import { thirdwebClient } from "../../../utils/sdk";
+import { badChainError } from "../../middleware/error";
 import { LocalFileStorage } from "../storage/localStorage";
 
 interface GetLocalWalletParams {
-  chainId: number;
-  walletAddress: string;
+	chainId: number;
+	walletAddress: string;
 }
 
 export const getLocalWallet = async ({
-  chainId,
-  walletAddress,
+	chainId,
+	walletAddress,
 }: GetLocalWalletParams) => {
-  let chain: Chain | undefined = undefined;
-  const config = await getConfig();
-  const CHAIN_OVERRIDES = config.chainOverrides;
-  try {
-    chain = await getChainByChainIdAsync(chainId);
-  } catch (error) {}
+	const chainV5 = await getChain(chainId);
+	const chain = await getChainMetadata(chainV5);
+	if (!chain) {
+		throw badChainError(chainId);
+	}
 
-  if (CHAIN_OVERRIDES) {
-    const parsedChainOverrides = JSON.parse(CHAIN_OVERRIDES);
-    const overrideChain = parsedChainOverrides.find(
-      (chainData: Static<typeof networkResponseSchema>) =>
-        chainData.chainId === chainId,
-    );
+	const wallet = new LocalWallet({ chain });
 
-    if (overrideChain) {
-      chain = overrideChain;
-    }
-  }
+	// TODO: Remove this with next breaking change
+	try {
+		// First, try to load the wallet using the encryption password
+		await wallet.load({
+			strategy: "encryptedJson",
+			password: env.ENCRYPTION_PASSWORD,
+			storage: new LocalFileStorage(walletAddress),
+		});
+	} catch {
+		// If that fails, try the thirdweb api secret key for backwards compatibility
+		await wallet.load({
+			strategy: "encryptedJson",
+			password: env.THIRDWEB_API_SECRET_KEY,
+			storage: new LocalFileStorage(walletAddress),
+		});
 
-  if (!chain) {
-    throw new Error(
-      `Invalid chain ${chainId}, please use a different value or provide Chain Override Data.`,
-    );
-  }
+		// If that works, save the wallet using the encryption password for the future
+		const walletDetails = await getWalletDetails({ address: walletAddress });
+		if (!walletDetails) {
+			throw new Error(
+				`Wallet details not found for wallet address ${walletAddress}`,
+			);
+		}
 
-  const wallet = new LocalWallet({ chain });
+		logger({
+			service: "worker",
+			level: "info",
+			message: `[Encryption] Updating local wallet ${walletAddress} to use ENCRYPTION_PASSWORD`,
+		});
 
-  // TODO: Remove this with next breaking change
-  try {
-    // First, try to load the wallet using the encryption password
-    await wallet.load({
-      strategy: "encryptedJson",
-      password: env.ENCRYPTION_PASSWORD,
-      storage: new LocalFileStorage(walletAddress),
-    });
-  } catch {
-    // If that fails, try the thirdweb api secret key for backwards compatibility
-    await wallet.load({
-      strategy: "encryptedJson",
-      password: env.THIRDWEB_API_SECRET_KEY,
-      storage: new LocalFileStorage(walletAddress),
-    });
+		await wallet.save({
+			strategy: "encryptedJson",
+			password: env.ENCRYPTION_PASSWORD,
+			storage: new LocalFileStorage(
+				walletAddress,
+				walletDetails.label ?? undefined,
+			),
+		});
+	}
 
-    // If that works, save the wallet using the encryption password for the future
-    const walletDetails = await getWalletDetails({ address: walletAddress });
-    if (!walletDetails) {
-      throw new Error(
-        `Wallet details not found for wallet address ${walletAddress}`,
-      );
-    }
-
-    logger({
-      service: "worker",
-      level: "info",
-      message: `[Encryption] Updating local wallet ${walletAddress} to use ENCRYPTION_PASSWORD`,
-    });
-
-    await wallet.save({
-      strategy: "encryptedJson",
-      password: env.ENCRYPTION_PASSWORD,
-      storage: new LocalFileStorage(
-        walletAddress,
-        walletDetails.label ?? undefined,
-      ),
-    });
-  }
-
-  return wallet;
+	return wallet;
 };
 
 export const getLocalWalletAccount = async (
-  walletAddress: Address,
+	walletAddress: Address,
 ): Promise<Account> => {
-  const json = await new LocalFileStorage(walletAddress).getItem("");
-  if (!json) {
-    throw new Error(`Wallet not found for address ${walletAddress}`);
-  }
+	const json = await new LocalFileStorage(walletAddress).getItem("");
+	if (!json) {
+		throw new Error(`Wallet not found for address ${walletAddress}`);
+	}
 
-  const wallet = await Wallet.fromEncryptedJson(
-    JSON.parse(json).data,
-    env.ENCRYPTION_PASSWORD,
-  );
+	const wallet = await Wallet.fromEncryptedJson(
+		JSON.parse(json).data,
+		env.ENCRYPTION_PASSWORD,
+	);
 
-  return privateKeyToAccount({
-    client: thirdwebClient,
-    privateKey: wallet.privateKey,
-  });
+	return privateKeyToAccount({
+		client: thirdwebClient,
+		privateKey: wallet.privateKey,
+	});
 };
