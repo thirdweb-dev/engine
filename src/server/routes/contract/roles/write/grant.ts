ddabd6b0cf9bac6900b1d4d57f5b055391e7f03c
Phillip Ho <arcoraven@gmail.com>
2024-09-26 11:59:08 +0800

    fix: broken imports (#678)

--- src/server/routes/contract/roles/write/grant.ts
+++ src/server/routes/contract/roles/write/grant.ts
@@ -1,4 +1,4 @@
-import { Type } from "@sinclair/typebox";
+import { Type, type Static } from "@sinclair/typebox";
 import type { FastifyInstance } from "fastify";
 import { StatusCodes } from "http-status-codes";
 import { queueTx } from "../../../../../db/transactions/queueTx";
