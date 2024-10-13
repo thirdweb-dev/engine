33cb5e347744206855dd43fbf77043fc677c3bf0
Joaquim Verges <joaquim.verges@gmail.com>
2024-10-14 10:51:33 +1300

    Handle unique OpenAPI tags in engine sdk

--- src/scripts/generate-sdk.ts
+++ src/scripts/generate-sdk.ts
@@ -23,8 +23,7 @@ async function main() {
 
     const code = fs
       .readFileSync("./sdk/src/Engine.ts", "utf-8")
-      .replace("export class Engine", "class EngineLogic")
-      .concat(`
+      .replace("export class Engine", "class EngineLogic").concat(`
 export class Engine extends EngineLogic {
   constructor(config: { url: string; accessToken: string; }) {
     super({ BASE: config.url, TOKEN: config.accessToken });
@@ -32,6 +31,22 @@ export class Engine extends EngineLogic {
 }
 `);
     fs.writeFileSync("./sdk/src/Engine.ts", code);
+
+    const tagsToReplace = ['erc20', 'erc721', 'erc1155'];
+    tagsToReplace.forEach((tag) => { 
+      const fileName = `${tag.charAt(0).toUpperCase() + tag.slice(1)}Service.ts`;
+      let code = fs.readFileSync(`./sdk/src/services/${fileName}`, "utf-8");
+      // replace erc methods to avoid duplication with the tag
+      // find all methods that start with 'ercX', remove 'ercX' from the name and lowercase the first letter
+      const regex = new RegExp(`public\\s+${tag}(\\w+)\\(`, 'g');
+      const methods = code.match(regex);
+      methods?.forEach((method) => {
+        const newMethod = method.replace(regex, (_, p1) => `public ${p1.charAt(0).toLowerCase() + p1.slice(1)}(`);
+        code = code.replace(method, newMethod);
+      });
+      fs.writeFileSync(`./sdk/src/services/${fileName}`, code);
+    });
+
   } catch (error) {
     console.error("Error:", error);
     kill(process.pid, "SIGINT");
