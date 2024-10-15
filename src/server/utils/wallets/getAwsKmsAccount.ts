29fa6976c23dfe62df539292a1e848642f92743d
Prithvish Baidya <deformercoding@gmail.com>
2024-10-16 03:25:59 +0530

    Refactor serialisation params in getAwsKmsAccount and getGcpKmsAccount (#731)
    * Refactor serialisation params in getAwsKmsAccount and getGcpKmsAccount
    
    * better typees

--- src/server/utils/wallets/getAwsKmsAccount.ts
+++ src/server/utils/wallets/getAwsKmsAccount.ts
@@ -52,15 +52,16 @@ export async function getAwsKmsAccount(
 
     const r = `0x${signature.r.toString("hex")}` as Hex;
     const s = `0x${signature.s.toString("hex")}` as Hex;
-    const v = signature.v;
+    const v = BigInt(signature.v);
 
-    const yParity = v % 2 === 0 ? 1 : (0 as 0 | 1);
+    const yParity: 0 | 1 = signature.v % 2 === 0 ? 1 : 0;
 
     const signedTx = serializeTransaction({
       transaction: tx,
       signature: {
         r,
         s,
+        v,
         yParity,
       },
     });
