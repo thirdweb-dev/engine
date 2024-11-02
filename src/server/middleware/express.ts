64a19861d70c3a15faf653c0b2b570696b11a2e0
Phillip Ho <arcoraven@gmail.com>
2024-11-02 11:04:19 +0800

    chore: remove unneeded express middleware

--- src/server/middleware/express.ts
+++
@@ -1,6 +0,0 @@
-import fastifyExpress from "@fastify/express";
-import { FastifyInstance } from "fastify";
-
-export const withExpress = async (server: FastifyInstance) => {
-  await server.register(fastifyExpress);
-};
