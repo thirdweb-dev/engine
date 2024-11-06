224bd8b701d6c478787658f68b3dad0c3a4bad36
Phillip Ho <arcoraven@gmail.com>
2024-11-07 05:06:28 +0800

    fix: Simplify Contract Subscriptions block range (#744)
    * fix: Simplify Contract Subscriptions block range
    
    * Fix build
    
    * logs
    
    * undo try/catch
    
    ---------
    
    Co-authored-by: Prithvish Baidya <deformercoding@gmail.com>

--- src/worker/tasks/chainIndexer.ts
+++ src/worker/tasks/chainIndexer.ts
@@ -1,135 +1,128 @@
-import { StaticJsonRpcBatchProvider } from "@thirdweb-dev/sdk";
-import { Address } from "thirdweb";
+import {
+  eth_blockNumber,
+  eth_getBlockByNumber,
+  getRpcClient,
+  type Address,
+} from "thirdweb";
 import { getBlockForIndexing } from "../../db/chainIndexers/getChainIndexer";
 import { upsertChainIndexer } from "../../db/chainIndexers/upsertChainIndexer";
 import { prisma } from "../../db/client";
 import { getContractSubscriptionsByChainId } from "../../db/contractSubscriptions/getContractSubscriptions";
-import { getSdk } from "../../utils/cache/getSdk";
+import { getChain } from "../../utils/chain";
 import { logger } from "../../utils/logger";
+import { thirdwebClient } from "../../utils/sdk";
 import { ProcessEventsLogQueue } from "../queues/processEventLogsQueue";
 import { ProcessTransactionReceiptsQueue } from "../queues/processTransactionReceiptsQueue";
 
-export const createChainIndexerTask = async (args: {
-  chainId: number;
-  maxBlocksToIndex: number;
-  toBlockOffset: number;
-}) => {
-  const { chainId, maxBlocksToIndex, toBlockOffset } = args;
+// A reasonable block range that is within RPC limits.
+// The minimum job time is 1 second, so this value should higher than the # blocks per second
+// on any chain to allow catching up if delayed.
+const MAX_BLOCK_RANGE = 500;
 
-  const chainIndexerTask = async () => {
-    try {
-      await prisma.$transaction(
-        async (pgtx) => {
-          let fromBlock;
-          try {
-            fromBlock = await getBlockForIndexing({ chainId, pgtx });
-          } catch (error) {
-            // row is locked, return
-            return;
-          }
+export const handleContractSubscriptions = async (chainId: number) => {
+  const chain = await getChain(chainId);
+  const rpcRequest = getRpcClient({
+    client: thirdwebClient,
+    chain,
+  });
 
-          const sdk = await getSdk({ chainId });
-          const provider = sdk.getProvider();
-          const currentBlockNumber =
-            (await provider.getBlockNumber()) - toBlockOffset;
+  await prisma.$transaction(
+    async (pgtx) => {
+      let fromBlock: number;
+      try {
+        fromBlock = await getBlockForIndexing({ chainId, pgtx });
+      } catch {
+        // row is locked, return
+        return;
+      }
 
-          // Limit toBlock to avoid hitting rate or block range limits when querying logs.
-          const toBlock = Math.min(
-            currentBlockNumber,
-            fromBlock + maxBlocksToIndex,
-          );
-
-          // No-op if fromBlock is already up-to-date.
-          if (fromBlock >= toBlock) {
-            return;
-          }
-
-          // Ensure that the block data exists.
-          // Sometimes the RPC nodes do not yet return data for the latest block.
-          const block = await provider.getBlockWithTransactions(toBlock);
-          if (!block) {
-            logger({
-              service: "worker",
-              level: "warn",
-              message: `Block data not available: ${toBlock} on chain: ${chainId}, url: ${
-                (provider as StaticJsonRpcBatchProvider).connection.url
-              }. Will retry in the next cycle.`,
-            });
-            return;
-          }
-
-          const contractSubscriptions = await getContractSubscriptionsByChainId(
-            chainId,
-            true,
-          );
-
-          // Identify contract addresses + event names to parse event logs, if any.
-          const eventLogFilters: {
-            address: Address;
-            events: string[];
-          }[] = contractSubscriptions
-            .filter((c) => c.processEventLogs)
-            .map((c) => ({
-              address: c.contractAddress as Address,
-              events: c.filterEvents,
-            }));
-          if (eventLogFilters.length > 0) {
-            await ProcessEventsLogQueue.add({
-              chainId,
-              fromBlock,
-              toBlock,
-              filters: eventLogFilters,
-            });
-          }
-
-          // Identify addresses + function names to parse transaction receipts, if any.
-          const transactionReceiptFilters: {
-            address: Address;
-            functions: string[];
-          }[] = contractSubscriptions
-            .filter((c) => c.processTransactionReceipts)
-            .map((c) => ({
-              address: c.contractAddress as Address,
-              functions: c.filterFunctions,
-            }));
-          if (transactionReceiptFilters.length > 0) {
-            await ProcessTransactionReceiptsQueue.add({
-              chainId,
-              fromBlock,
-              toBlock,
-              filters: transactionReceiptFilters,
-            });
-          }
-
-          // Update the latest block number.
-          try {
-            await upsertChainIndexer({
-              pgtx,
-              chainId,
-              currentBlockNumber: toBlock,
-            });
-          } catch (error) {
-            logger({
-              service: "worker",
-              level: "error",
-              message: `Failed to update latest block number - Chain Indexer: ${chainId}`,
-              error: error,
-            });
-          }
-        },
-        {
-          timeout: 60 * 1000, // 1 minute timeout
-        },
+      // Cap the range to avoid hitting rate limits or block range limits from RPC.
+      const latestBlockNumber = await eth_blockNumber(rpcRequest);
+      const toBlock = Math.min(
+        Number(latestBlockNumber),
+        fromBlock + MAX_BLOCK_RANGE,
       );
-    } catch (err: any) {
-      logger({
-        service: "worker",
-        level: "error",
-        message: `Failed to index: ${chainId}`,
-        error: err,
-      });
-    }
-  };
 
-  return chainIndexerTask;
+      // No-op if fromBlock is already up-to-date.
+      if (fromBlock >= toBlock) {
+        return;
+      }
+
+      // Ensure that the block data exists.
+      // Sometimes the RPC nodes do not yet return data for the latest block.
+      const block = await eth_getBlockByNumber(rpcRequest, {
+        blockNumber: BigInt(toBlock),
+      });
+      if (!block) {
+        logger({
+          service: "worker",
+          level: "warn",
+          message: `Block ${toBlock} data not available on chain ${chainId} yet.`,
+        });
+        return;
+      }
+
+      const contractSubscriptions = await getContractSubscriptionsByChainId(
+        chainId,
+        true,
+      );
+
+      // Identify contract addresses + event names to parse event logs, if any.
+      const eventLogFilters: {
+        address: Address;
+        events: string[];
+      }[] = contractSubscriptions
+        .filter((c) => c.processEventLogs)
+        .map((c) => ({
+          address: c.contractAddress as Address,
+          events: c.filterEvents,
+        }));
+      if (eventLogFilters.length > 0) {
+        await ProcessEventsLogQueue.add({
+          chainId,
+          fromBlock,
+          toBlock,
+          filters: eventLogFilters,
+        });
+      }
+
+      // Identify addresses + function names to parse transaction receipts, if any.
+      const transactionReceiptFilters: {
+        address: Address;
+        functions: string[];
+      }[] = contractSubscriptions
+        .filter((c) => c.processTransactionReceipts)
+        .map((c) => ({
+          address: c.contractAddress as Address,
+          functions: c.filterFunctions,
+        }));
+      if (transactionReceiptFilters.length > 0) {
+        await ProcessTransactionReceiptsQueue.add({
+          chainId,
+          fromBlock,
+          toBlock,
+          filters: transactionReceiptFilters,
+        });
+      }
+
+      // Update the latest block number.
+      try {
+        await upsertChainIndexer({
+          pgtx,
+          chainId,
+          currentBlockNumber: toBlock,
+        });
+      } catch (error) {
+        logger({
+          service: "worker",
+          level: "error",
+          message: `Updating latest block number on chain ${chainId}`,
+          error,
+        });
+      }
+    },
+    {
+      timeout: 60 * 1000,
+    },
+  );
 };
