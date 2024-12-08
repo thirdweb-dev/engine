a3b1ef5f42a80d278521d2db0690a4439d452d31
Phillip Ho <arcoraven@gmail.com>
2024-12-08 15:19:21 +0800

    fix metadata

--- src/server/routes/contract/extensions/erc721/write/mintTo.ts
+++ src/server/routes/contract/extensions/erc721/write/mintTo.ts
@@ -3,6 +3,7 @@ import type { FastifyInstance } from "fastify";
 import { StatusCodes } from "http-status-codes";
 import { getContract } from "thirdweb";
 import { mintTo } from "thirdweb/extensions/erc721";
+import { NFTInput } from "thirdweb/utils";
 import { getChain } from "../../../../../../utils/chain";
 import { thirdwebClient } from "../../../../../../utils/sdk";
 import { queueTransaction } from "../../../../../../utils/transaction/queueTransation";
@@ -88,10 +89,24 @@ export async function erc721mintTo(fastify: FastifyInstance) {
         address: contractAddress,
       });
 
+      // Backward compatibility: This endpoint body uses the v4 SDK shape.
+      // Transform to v5 SDK shape to use the v5 endpoint.
+      const nft: NFTInput | string =
+        typeof metadata === "string"
+          ? metadata
+          : {
+              name: metadata.name,
+              description: metadata.description,
+              image: metadata.image,
+              animation_url: metadata.animation_url,
+              external_url: metadata.external_url,
+              background_color: metadata.background_color,
+              properties: metadata.properties,
+            };
       const transaction = mintTo({
         contract,
         to: receiver,
-        nft: typeof metadata === "string" ? metadata : { nft: metadata },
+        nft,
       });
 
       const queueId = await queueTransaction({
