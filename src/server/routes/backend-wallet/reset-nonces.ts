b468f5dcbbb85b44bd9b6535a7db2486aa518e22
Phillip Ho <arcoraven@gmail.com>
2025-01-25 11:45:12 +0800

    fix: /reset-nonces endpoint allows deleting nonce state without resyncing (#836)
    * chore: Allow deleting stored nonces without resyncing yet
    
    * rename
    
    * also delete nonce-history

--- src/server/routes/backend-wallet/reset-nonces.ts
+++ src/server/routes/backend-wallet/reset-nonces.ts
@@ -21,6 +21,11 @@ const requestBodySchema = Type.Object({
     description:
       "The backend wallet address to reset nonces for. Omit to reset all backend wallets.",
   }),
+  syncOnchainNonces: Type.Boolean({
+    description:
+      "Resync nonces to match the onchain transaction count for your backend wallets. (Default: true)",
+    default: true,
+  }),
 });
 
 const responseSchema = Type.Object({
@@ -61,7 +66,11 @@ export const resetBackendWalletNoncesRoute = async (
       },
     },
     handler: async (req, reply) => {
-      const { chainId, walletAddress: _walletAddress } = req.body;
+      const {
+        chainId,
+        walletAddress: _walletAddress,
+        syncOnchainNonces,
+      } = req.body;
 
       // If chain+wallet are provided, only process that wallet.
       // Otherwise process all used wallets that has nonce state.
@@ -70,19 +79,21 @@ export const resetBackendWalletNoncesRoute = async (
           ? [{ chainId, walletAddress: getAddress(_walletAddress) }]
           : await getUsedBackendWallets();
 
-      const RESYNC_BATCH_SIZE = 50;
-      for (let i = 0; i < backendWallets.length; i += RESYNC_BATCH_SIZE) {
-        const batch = backendWallets.slice(i, i + RESYNC_BATCH_SIZE);
+      const BATCH_SIZE = 50;
+      for (let i = 0; i < backendWallets.length; i += BATCH_SIZE) {
+        const batch = backendWallets.slice(i, i + BATCH_SIZE);
 
         // Delete nonce state for these backend wallets.
         await deleteNoncesForBackendWallets(backendWallets);
 
-        // Resync nonces for these backend wallets.
-        await Promise.allSettled(
-          batch.map(({ chainId, walletAddress }) =>
-            syncLatestNonceFromOnchain(chainId, walletAddress),
-          ),
-        );
+        if (syncOnchainNonces) {
+          // Resync nonces for these backend wallets.
+          await Promise.allSettled(
+            batch.map(({ chainId, walletAddress }) =>
+              syncLatestNonceFromOnchain(chainId, walletAddress),
+            ),
+          );
+        }
       }
 
       reply.status(StatusCodes.OK).send({
