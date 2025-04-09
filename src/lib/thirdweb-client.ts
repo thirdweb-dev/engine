8a1693e6a68c2a19aef4ce0e812cabb30536a824
Joaquim Verges <joaquim.verges@gmail.com>
2025-04-09 20:09:53 +1200

    always set domains

--- src/lib/thirdweb-client.ts
+++ src/lib/thirdweb-client.ts
@@ -10,17 +10,14 @@ import {
   THIRDWEB_STORAGE_DOMAIN,
 } from "../constants/urls.js";
 
-if (env.NODE_ENV !== "production") {
-  // if not on production: run this when creating a client to set the domains
-  setThirdwebDomains({
-    rpc: THIRDWEB_RPC_DOMAIN,
-    inAppWallet: THIRDWEB_INAPP_WALLET_DOMAIN,
-    pay: THIRDWEB_PAY_DOMAIN,
-    storage: THIRDWEB_STORAGE_DOMAIN,
-    social: THIRDWEB_SOCIAL_API_DOMAIN,
-    bundler: THIRDWEB_BUNDLER_DOMAIN,
-  });
-}
+setThirdwebDomains({
+  rpc: THIRDWEB_RPC_DOMAIN,
+  inAppWallet: THIRDWEB_INAPP_WALLET_DOMAIN,
+  pay: THIRDWEB_PAY_DOMAIN,
+  storage: THIRDWEB_STORAGE_DOMAIN,
+  social: THIRDWEB_SOCIAL_API_DOMAIN,
+  bundler: THIRDWEB_BUNDLER_DOMAIN,
+});
 
 export const thirdwebClient = createThirdwebClient({
   secretKey: env.THIRDWEB_API_SECRET_KEY,
--- src/server/middleware/thirdweb-client.ts
+++ src/server/middleware/thirdweb-client.ts
@@ -15,17 +15,15 @@ export const thirdwebClientMiddleware = createMiddleware(async (c, next) => {
   const thirdwebSecretKey = c.req.header("x-thirdweb-secret-key");
 
   if (thirdwebSecretKey) {
-    if (env.NODE_ENV !== "production") {
       // if not on production: run this when creating a client to set the domains
-      setThirdwebDomains({
-        rpc: THIRDWEB_RPC_DOMAIN,
-        inAppWallet: THIRDWEB_INAPP_WALLET_DOMAIN,
-        pay: THIRDWEB_PAY_DOMAIN,
-        storage: THIRDWEB_STORAGE_DOMAIN,
-        social: THIRDWEB_SOCIAL_API_DOMAIN,
-        bundler: THIRDWEB_BUNDLER_DOMAIN,
-      });
-    }
+    setThirdwebDomains({
+      rpc: THIRDWEB_RPC_DOMAIN,
+      inAppWallet: THIRDWEB_INAPP_WALLET_DOMAIN,
+      pay: THIRDWEB_PAY_DOMAIN,
+      storage: THIRDWEB_STORAGE_DOMAIN,
+      social: THIRDWEB_SOCIAL_API_DOMAIN,
+      bundler: THIRDWEB_BUNDLER_DOMAIN,
+    });
     const thirdwebClient = createThirdwebClient({
       secretKey: thirdwebSecretKey,
     });
